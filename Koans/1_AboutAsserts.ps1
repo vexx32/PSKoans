d6fb649a64c0fef638995cccb084e6a4f3dc411f
Joel <jfrancis@heritageflooring.com>
2018-07-05 20:47:13 -0400

    Add initial koan for equalities

---
+++ Koans/1_AboutAsserts.ps1
@@ -0,0 +1,28 @@
+<#
+    Getting Started
+    
+    The PowerShell Koans are a set of exercises designed to get you familiar 
+    with PowerShell. By the time you're done, you'll have a basic 
+    understanding of the syntax of PoSH and learn a little more
+    about scripting in general.
+    
+    Answering Problems
+    
+    This is where the fun begins! Each koan contains an example designed 
+    to teach you a lesson about PowerShell. If you execute the program 
+    defined in this project, you will get a message that the koan below 
+    has failed. Your job is to fill in the blank (the __ symbol) to 
+    make it pass. Once you make the change, re-run the program to 
+    make sure the koan passes, and continue on to the next failing koan.
+    With each  passing koan, you'll learn more about PowerShell, and 
+    add another weapon to your PowerShell scripting arsenal.
+#>
+Describe "Equality" {
+    $ExpectedValue = 1 + 1
+    $ActualValue = __ # Start by changing this line
+
+    $ActualValue | Should -Be $ExpectedValue
+
+    # Easy, right? Try one more!
+    (__ + 2 -eq 3) | Should -Be $true
+}
