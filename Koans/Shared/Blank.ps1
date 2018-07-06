7501ec63269a72d6e0d82349413160f55162626f
Joel <jfrancis@heritageflooring.com>
2018-07-05 22:47:42 -0400

    Return $null rather than $false

--- Koans/Shared/Blank.ps1
+++ Koans/Shared/Blank.ps1
@@ -1,5 +1,16 @@
+<#
+.SYNOPSIS
+Returns $null.
+
+.DESCRIPTION
+That's it, it just gives you nothing.
+
+.EXAMPLE
+PS> __ -eq $null
+True
+#>
 function Get-Blank {
     [Alias('__')]
     param()
-    $false
+    $null
 }
