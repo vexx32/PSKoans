1d19d936940ccbce1f01bf30724bd46637e23c69
Joel <jfrancis@heritageflooring.com>
2018-07-07 19:45:01 -0400

    Complete basic functions and script blocks.

--- Koans/3_AboutFunctions.Tests.ps1
+++ Koans/3_AboutFunctions.Tests.ps1
@@ -44,11 +44,19 @@ Describe "Functions" {
 
     It "can accept parameters" {
         # Simplicity is a two-sided coin.
+        function Add-Things {
+            return (args[0] + args[1])
+        }
+
+        # Values are supplied separated by spaces, without parentheses.
+        Add-Things "7" __ | Should -Be "72" # Strings add differently than numbers!
+        Add-Things 1 2 | Should -Be __
+
+        # The road to mastery is travelled with many small steps.
         function Add-Numbers ($Number1, $Number2) {
             return ($Number1 + $Number2)
         }
 
-        # Values are supplied separated by spaces, without parentheses.
         Add-Numbers 1 7 | Should -Be __
         Add-Numbers __ 15 | Should -Be 31
         
@@ -59,11 +67,33 @@ Describe "Functions" {
     It "can declare parameters explicitly" {
         function Measure-String {
             # Declaring parameters in this way allows for more advanced techniques,
-            # which will be covered in Advanced Functions
+            # which will be covered in AboutAdvancedFunctions
             param(
                 $InputString
             )
             return $InputString.Length
         }
+        # How long is a piece of string?
+        Measure-String "Hello!" | Should -Be 6
+        Measure-String "Muffins" | Should -Be __
+    }
+
+    It "is a named script block" {
+        <#
+            Script blocks can be used to group commands without defining a function.
+            These can be used for various things, most commonly for parameters or
+            defining a sequence of actions to be executed multiple times.
+        #>
+        $Script = {
+            throw "This is a script block that just throws an error."
+        }
+
+        # Sometimes, you just want to watch things burn, and the console to stream red errors.
+        $Script2 = {
+            # Currently, this will just output the script block as an object
+            $Script # Try appending .Invoke() to this line to cause the original block to be executed
+        }
+        $Script | Should -Throw
+        $Script2 | Should -Throw
     }
 }
