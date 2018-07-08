5cadbc902efbe7cb169aef9a71d60638af3f020c
Joel <jfrancis@heritageflooring.com>
2018-07-08 17:37:31 -0400

    Actually getting started on the point of the branch now lol

---
+++ Koans/6_AboutOperators.Tests.ps1
@@ -0,0 +1,12 @@
+<#
+    Operators
+
+    Just like many other programming languages, PowerShell has special operators designed to work with data.
+    
+    You can use the following command to get a full overview of how operators work:
+
+        Get-Help about_Operators
+
+    Loosely, operators fall into a few categories: assignment, arithmetic, comparison, redirection, and
+    string operators.
+#>
