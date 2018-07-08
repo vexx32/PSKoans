6963c7dad0c2fed696411fe9dbbfb90453d3b63f
Joel <jfrancis@heritageflooring.com>
2018-07-08 16:54:33 -0400

    Refactor Invoke-PSKoans.ps1 Oops, got sidetracked. Found a way to provide more helpful output than standard Pester syntax. ++Zen

--- PSKoans.ps1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Path "$PSScriptRoot\Koans" -Filter '*.Tests.ps1' |
-    ForEach-Object {
-        $KoansFailed = Invoke-Pester -Script $_.FullName
-        if (-not $KoansFailed) {
-            break
-        }
-    }
