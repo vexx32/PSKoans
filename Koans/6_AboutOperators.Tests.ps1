8eaac557a76a49de9f18ccc8bb872066a1bc2304
Joel <jfrancis@heritageflooring.com>
2018-07-08 18:23:13 -0400

    Complete addition section, begin subtraction

--- Koans/6_AboutOperators.Tests.ps1
+++ Koans/6_AboutOperators.Tests.ps1
@@ -14,19 +14,47 @@
     In terms of order of operations, arithmetic operators typically execute before comparison operators,
     followed by string operators, redirection operators, and finally assignment operators execute last.
 #>
-Describe "Assignment and Arithmetic Operators" {
-    Describe "Addition Operator" {
-        It "is used to add two items together" {
-            13 + 4 | Should -Be 17
-            __ + 6 | Should -Be 13
-        }
-        It "can be used to concatenate strings" {
-            "hello" + "world" | Should -Be __
-            "My name is " + "Jim" | Should -Be "My name is Jim"
-        }
-        It ""
+Describe "Addition" {
+    It "is used to add two items together" {
+        13 + 4 | Should -Be 17
+        __ + 6 | Should -Be 13
+        13.7 + 4 | Should -Be __
+    }
+    It "can be used to concatenate strings" {
+        # Be aware that it does not add any extra spacing, merely joins the exact strings.
+        "hello" + "world" | Should -Be __
+        "My name is " + "Jim" | Should -Be "My name is Jim"
+    }
+    It "can be used to create arrays" {
+        # As we covered in AboutArrays, this is not so much "adding" arrays together as it is
+        # building a totally new array. It does, however, have its uses.
+        $Array = 1, 2, 3, 4, 5
+        $NewArray = $Array + 7
+
+        __ | Should -Be $NewArray
+    }
+    It "behaves according to the type of the left-hand item" {
+        "10.5" + 11 | Should -Be 21.5 # Shouldn't it? What do you think?
+        # Interestingly, if the left type is numeric, an appropriate type will be chosen by
+        # taking both operands into account
+        
+        11 + "12.5" | Should -Be __
+
+        # Adding items into typed arrays will also cause the resulting value to be converted
+        [int[]]@(1, 2, 3, 4, 5) + "17" | Should -Be __
     }
 }
+Describe "Subtraction" {
+    It "works similarly to addition" {
+
+    }
+    It "cannot be used with strings" {
+        {"hello" - "h"} | Should -Throw
+        # Except, of course, when the string contains a useable number
+        "12" - "7.5" | Should -Be __
+    }
+}
+
 Describe "Comparison Operators" {
 
 }
