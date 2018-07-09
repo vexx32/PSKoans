b986a8b747a81c400ae822dff0a12fa9324fdf28
Joel <jfrancis@heritageflooring.com>
2018-07-09 12:22:47 -0400

    Nested Describes for operators would be better I think

--- Koans/1_Foundations/6_AboutOperators.Tests.ps1
+++ Koans/1_Foundations/6_AboutOperators.Tests.ps1
@@ -22,85 +22,87 @@
     These can be used for standard arithmetic with numerical values, as well as some limited
     usage with arrays and strings that can come in handy.
 #>
-Describe "Assignment Operator" {
-    It "is used to assign a value to variables" {
+Describe 'Assignment Operator' {
+    It 'is used to assign a value to variables' {
         $ExpectedValue = 1 + 1
         $ActualValue = __
 
         $ActualValue | Should -Be $ExpectedValue
     }
-    It "is also used to assign a value to properties or elements" {
+    It 'is also used to assign a value to properties or elements' {
         # With arrays, we can assign values directly to indexes
         $Array = 1, 2, 3
         $Array[1] = 5
         $Array | Should -Be @(1, 2, 3) # What would change?
     }
 }
-Describe 'Addition' {
-    It 'is used to add two items together' {
-        13 + 4 | Should -Be 17
-        __ + 6 | Should -Be 13
-        13.7 + 4 | Should -Be __
-    }
-    It 'can be used to concatenate strings' {
-        # Be aware that it does not add any extra spacing, merely joins the exact strings.
-        'hello' + 'world' | Should -Be __
-        'My name is ' + 'Jim' | Should -Be 'My name is Jim'
-    }
-    It 'can be used to create arrays' {
-        # As we covered in AboutArrays, this is not so much 'adding' arrays together as it is
-        # building a totally new array. It does, however, have its uses.
-        $Array = 1, 2, 3, 4, 5
-        $NewArray = $Array + 7
+Describe 'Arithmetic Operators' {
+    Describe 'Addition' {
+        It 'is used to add two items together' {
+            13 + 4 | Should -Be 17
+            __ + 6 | Should -Be 13
+            13.7 + 4 | Should -Be __
+        }
+        It 'can be used to concatenate strings' {
+            # Be aware that it does not add any extra spacing, merely joins the exact strings.
+            'hello' + 'world' | Should -Be __
+            'My name is ' + 'Jim' | Should -Be 'My name is Jim'
+        }
+        It 'can be used to create arrays' {
+            # As we covered in AboutArrays, this is not so much 'adding' arrays together as it is
+            # building a totally new array. It does, however, have its uses.
+            $Array = 1, 2, 3, 4, 5
+            $NewArray = $Array + 7
 
-        __ | Should -Be $NewArray
-    }
-    It 'behaves according to the type of the left-hand item' {
-        '10.5' + 11 | Should -Be 21.5 # Shouldn't it? What do you think?
-        # Interestingly, if the left type is numeric, an appropriate type will be chosen by
-        # taking both operands into account
+            __ | Should -Be $NewArray
+        }
+        It 'behaves according to the type of the left-hand item' {
+            '10.5' + 11 | Should -Be 21.5 # Shouldn't it? What do you think?
+            # Interestingly, if the left type is numeric, an appropriate type will be chosen by
+            # taking both operands into account
         
-        11 + '12.5' | Should -Be __
-        12.21 + 'FILL_ME_IN' -eq 23.43 | Should -BeTrue
+            11 + '12.5' | Should -Be __
+            12.21 + 'FILL_ME_IN' -eq 23.43 | Should -BeTrue
 
-        # Adding items into typed arrays will also cause the resulting value to be converted
-        [int[]]@(1, 2, 3, 4, 5) + '17' | Should -Be __
+            # Adding items into typed arrays will also cause the resulting value to be converted
+            [int[]]@(1, 2, 3, 4, 5) + '17' | Should -Be __
+        }
     }
-}
-Describe 'Subtraction' {
-    It 'works similarly to addition' {
-        12 - 7 | Should -Be 5
-        11 - 3.5 | Should -Be __
-    }
-    It 'cannot be used with strings' {
-        {'hello' - 'h'} | Should -Throw
+    Describe 'Subtraction' {
+        It 'works similarly to addition' {
+            12 - 7 | Should -Be 5
+            11 - 3.5 | Should -Be __
+        }
+        It 'cannot be used with strings' {
+            {'hello' - 'h'} | Should -Throw
 
-        # Except, of course, when the string contains a useable number.
-        '12' - '7.5' | Should -Be __
+            # Except, of course, when the string contains a useable number.
+            '12' - '7.5' | Should -Be __
 
-        # In other words, subtraction only operates on numerical values.
-        {@(1, 2) - 1} | Should -Throw
+            # In other words, subtraction only operates on numerical values.
+            {@(1, 2) - 1} | Should -Throw
+        }
     }
-}
-Describe 'Multiplication' {
-    It 'can be used on both integer and non-integer numerals' {
-        12 * 4 | Should -Be __
-        12.1 * 2 | Should -Be 24.2
+    Describe 'Multiplication' {
+        It 'can be used on both integer and non-integer numerals' {
+            12 * 4 | Should -Be __
+            12.1 * 2 | Should -Be 24.2
+        }
+        It 'can also be used on strings' {
+            'A' * 4 -eq 'FILL_ME_IN' | Should -BeTrue
+            __ * 4 -eq "NANANANA" | Should -BeTrue
+        }
     }
-    It 'can also be used on strings' {
-        'A' * 4 -eq 'FILL_ME_IN' | Should -BeTrue
-        __ * 4 -eq "NANANANA" | Should -BeTrue
-    }
-}
-Describe "Division" {
-    It "is restricted to numeric use only" {
-        # As with subtraction, there's no useful meaning of using division on a string
-        # so any attempts to do so will throw an error.
-        {"hello!" / 3} | Should -Throw
-        # Unlike with other numerical operators, however, division often results
-        # in a non-integer (double) value even when both operands are integers.
-        3 / 4 | Should -Be 0.75
-        __ / 10 -eq 0.5 | Should -BeTrue
+    Describe "Division" {
+        It "is restricted to numeric use only" {
+            # As with subtraction, there's no useful meaning of using division on a string
+            # so any attempts to do so will throw an error.
+            {"hello!" / 3} | Should -Throw
+            # Unlike with other numerical operators, however, division often results
+            # in a non-integer (double) value even when both operands are integers.
+            3 / 4 | Should -Be 0.75
+            __ / 10 -eq 0.5 | Should -BeTrue
+        }
     }
 }
 <#
