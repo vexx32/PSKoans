8b8e8ba47e884f0e186b6f264558b8694db1dcad
Joel <jfrancis@heritageflooring.com>
2018-07-08 23:42:48 -0400

    Refactor module code to move messy write-host code to private function.

--- PSKoans.psm1
+++ PSKoans.psm1
@@ -17,13 +17,8 @@ function Invoke-PSKoans {
             exit
         }
 
-        Write-Host -ForegroundColor Cyan @"
-    Welcome, seeker of enlightenment. 
-    Please wait a moment while we examine your karma...
+        Write-MeditationPrompt -Greeting
 
-"@
-        $Red = @{ForegroundColor = "Red"}
-        $Blue = @{ForegroundColor = "Cyan"}
         $PesterTestCount = Invoke-Pester -PassThru -Show None | Select-Object -ExpandProperty TotalCount
         $Tests = Get-ChildItem -Path "$PSScriptRoot\Koans" -Filter '*.Tests.ps1' -Recurse
         $KoansPassed = 0
@@ -43,53 +38,110 @@ function Invoke-PSKoans {
                 Where-Object Result -eq 'Failed' |
                 Select-Object -First 1
         
-            Write-Host @Red @"
-    {Describe "$($NextKoanFailed.Describe)"} has damaged your karma.
+            $Meditation = @{
+                DescribeName = $NextKoanFailed.Describe
+                Expectation  = $NextKoanFailed.ErrorRecord
+                ItName       = $NextKoanFailed.Name
+                Meditation   = $NextKoanFailed.StackTrace
+                KoansPassed  = $KoansPassed
+                TotalKoans   = $PesterTestCount
+            }
+            Write-MeditationPrompt @Meditation
+        }
+    }
+}
+function Get-Blank {
+    [Alias('__', 'FILL_ME_IN')]
+    param()
+    $null
+}
+function Write-MeditationPrompt {
+    [CmdletBinding(DefaultParameterSetName = 'Meditation')]
+    param(
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $DescribeName,
+
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Expectation,
+        
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ItName,
+        
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Meditation,
+
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNull()]
+        [int]
+        $KoansPassed,
+
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNull()]
+        [int]
+        $TotalKoans,
+
+        [Parameter(Mandatory, ParameterSetName = 'Greeting')]
+        [switch]
+        $Greeting
+    )
+
+    $Red = @{ForegroundColor = "Red"}
+    $Blue = @{ForegroundColor = "Cyan"}
+
+    if ($PSCmdlet.ParameterSetName -eq 'Greeting') {
+        Write-Host -ForegroundColor Cyan @"
+    Welcome, seeker of enlightenment. 
+    Please wait a moment while we examine your karma...
+
 "@
-            Write-Host @Blue @"
+        return
+    }
+    Write-Host @Red @"
+    {Describe "$DescribeName"} has damaged your karma.
+"@
+    Write-Host @Blue @"
 
     You have not yet reached enlightenment.
     
     The answers you seek...
 "@
-            Write-Host @Red @"
-    $($NextKoanFailed.ErrorRecord)
-"@
-            Write-Host @Blue @"
+    Write-Host @Red $Expectation
+    Write-Host @Blue @"
     
     Please meditate on the following code:
 "@
-            Write-Host @Red @"
-    [It] $($NextKoanFailed.Name)
-    $($NextKoanFailed.StackTrace)
+    Write-Host @Red @"
+    [It] $ItName
+    $Meditation
 "@
-            Write-Host @Blue @"
+    Write-Host @Blue @"
 
     Mountains are merely mountains.
         
     Your path thus far: 
 
 "@
-            $ProgressAmount = "$KoansPassed/$PesterTestCount"
-            $ProgressWidth = $host.UI.RawUI.WindowSize.Width - (3 + $ProgressAmount.Length)
-            $PortionDone = ($KoansPassed / $PesterTestCount) * $ProgressWidth
+    $ProgressAmount = "$KoansPassed/$TotalKoans"
+    $ProgressWidth = $host.UI.RawUI.WindowSize.Width - (3 + $ProgressAmount.Length)
+    $PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
 
-            "[{0}{1}] {2}" -f @(
-                "$([char]0x25a0)" * $PortionDone
-                "$([char]0x2015)" * ($ProgressWidth - $PortionDone)
-                $ProgressAmount
-            ) | Write-Host @Blue
+    "[{0}{1}] {2}" -f @(
+        "$([char]0x25a0)" * $PortionDone
+        "$([char]0x2015)" * ($ProgressWidth - $PortionDone)
+        $ProgressAmount
+    ) | Write-Host @Blue
 
-            Write-Host @Blue @"
+    Write-Host @Blue @"
     
     You may run 'rake -Meditate' to begin your meditation.
 
 "@
-        } # end if
-    } # end Process
-} # end function
-function Get-Blank {
-    [Alias('__', 'FILL_ME_IN')]
-    param()
-    $null
 }
