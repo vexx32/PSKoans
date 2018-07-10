e90f045048b54018be0c13399eddfd0df822305b
Joel <jfrancis@heritageflooring.com>
2018-07-10 12:41:18 -0400

    Finish If/Else conditionals

--- Koans/1_Foundations/9_AboutConditionals.Tests.ps1
+++ Koans/1_Foundations/9_AboutConditionals.Tests.ps1
@@ -8,5 +8,49 @@
     data, as is usually seen in functional programming languages.
 #>
 Describe 'Conditionals' {
-    
+    Describe 'If/Else' {
+        It "is used to alter which code should be executed based on input" {
+            function Assert-IsEven ($Number) {
+                if ($Number % 2 -eq 0) {
+                    # Values or objects dropped to output on conditionals will still
+                    # be used as output for the function.
+                    "EVEN"
+                }
+                else {
+                    "ODD"
+                }
+            }
+        
+            Assert-IsEven -Number 2 | Should -Be "__"
+            Assert-IsEven -Number __ | Should -Be "ODD"
+        }
+        It "can be used alone" {
+            function Set-Number ($Number) {
+                if ($Number -gt 5) {
+                    $Number * 10
+                }
+                # What happens if the conditional completely skips the if statement?
+            }
+            Set-Number -Number 4 | Should -Be __
+            Set-Number -Number __ | Should -Be 70
+        }
+        It "can be used to select a value for a variable" {
+            function Get-Thing {
+                return (Get-ChildItem -Path $env:TEMP | Select-Object -Skip 3).Length
+            }
+            $Thing = Get-Thing
+            $Result = if ($Thing.Length -gt 5) {
+                # The item dropped to output is stored in the assigned variable
+                $Thing
+            }
+            else {
+                # but depending on the outcome of the conditional, either could end up stored!
+                "$Thing is less than 5"
+            }
+            $Result | Should -Be __
+        }
+    }
+    Describe 'Switch' {
+
+    }
 }
