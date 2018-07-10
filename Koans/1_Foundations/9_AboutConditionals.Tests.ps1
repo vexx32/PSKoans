164a448bff7289a9b65567e5b7fa6d9df787d1be
Joel <jfrancis@heritageflooring.com>
2018-07-10 12:24:14 -0400

    Begin work on conditionals

---
+++ Koans/1_Foundations/9_AboutConditionals.Tests.ps1
@@ -0,0 +1,12 @@
+<#
+    Conditionals and Branching
+
+    Conditionals operate with language keywords like if, else, and switch, and utilise 
+    boolean statements to determine program control flow.
+
+    Due to how PowerShell handles output, its conditional statements can return output 
+    data, as is usually seen in functional programming languages.
+#>
+Describe 'Conditionals' {
+    
+}
