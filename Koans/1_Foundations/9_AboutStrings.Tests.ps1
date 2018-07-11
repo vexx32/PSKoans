c2bd8ad975c43a1abbbe96923f68db72c940aa71
VALDINGER <svalding@kent.edu>
2018-07-11 00:24:30 -0400

    Added some more string tests

--- Koans/1_Foundations/9_AboutStrings.Tests.ps1
+++ Koans/1_Foundations/9_AboutStrings.Tests.ps1
@@ -1,3 +1,8 @@
+<#
+    Strings
+
+
+#>
 Describe "Strings" {
     It "Is a simple string" {
         "string" | Should -Be __
@@ -18,5 +23,36 @@ Describe "Strings" {
     It "Can expand variables" {
         $var = "apple"
         "My favorite fruit is $var" | Should -Be __
+    }
+
+    Describe "Strings can contain methods"{
+        $file = Get-ChildItem C:\Windows | Select-Object -ExpandProperty FullName
+
+        It "Can do a simple expansion" {
+        "The windows directory is located here: $file" | Should -Be __
+        }
+
+        It "Handles other ways of doing the same thing" {
+
+            "The windows directory is located $(Get-ChildItem C:\Windows | Select-Object -ExpandProperty FullName)" |
+            Should -Be __
+        }
+
+        Describe "Strings can be concatenated" {
+           It "Adds strings together" {
+            $string1 = "This string"
+            $string2 = "is cool."
+            $sentence = $string1 + $string2 | Should -Be "This string is cool."
+
+            It "Can be done simpler" {
+                $string1 = "This string"
+                $string2 = "is cool."
+                __ | Should -Be "This string is cool"
+            }
+           
+        }
+        }
+
+
     }
 }
