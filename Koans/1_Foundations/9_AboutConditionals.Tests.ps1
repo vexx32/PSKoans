53b7f015c281ffb6720c96908311f11b65728e9b
Joel <jfrancis@heritageflooring.com>
2018-07-10 23:31:53 -0400

    Complete first It statement for switches

--- Koans/1_Foundations/9_AboutConditionals.Tests.ps1
+++ Koans/1_Foundations/9_AboutConditionals.Tests.ps1
@@ -9,22 +9,22 @@
 #>
 Describe 'Conditionals' {
     Describe 'If/Else' {
-        It "is used to alter which code should be executed based on input" {
+        It 'is used to alter which code should be executed based on input' {
             function Assert-IsEven ($Number) {
                 if ($Number % 2 -eq 0) {
                     # Values or objects dropped to output on conditionals will still
                     # be used as output for the function.
-                    "EVEN"
+                    'EVEN'
                 }
                 else {
-                    "ODD"
+                    'ODD'
                 }
             }
         
-            Assert-IsEven -Number 2 | Should -Be "__"
-            Assert-IsEven -Number __ | Should -Be "ODD"
+            Assert-IsEven -Number 2 | Should -Be '__'
+            Assert-IsEven -Number __ | Should -Be 'ODD'
         }
-        It "can be used alone" {
+        It 'can be used alone' {
             function Set-Number ($Number) {
                 if ($Number -gt 5) {
                     $Number * 10
@@ -34,7 +34,7 @@ Describe 'Conditionals' {
             Set-Number -Number 4 | Should -Be __
             Set-Number -Number __ | Should -Be 70
         }
-        It "can be used to select a value for a variable" {
+        It 'can be used to select a value for a variable' {
             function Get-Thing {
                 return (Get-ChildItem -Path $env:TEMP | Select-Object -Skip 3).Length
             }
@@ -51,23 +51,41 @@ Describe 'Conditionals' {
         }
     }
     Describe 'Switch' {
-        It "is used to create multiple possible branches" {
+        # Switches often replace stacks of if/elseif/../else conditionals
+        # If you have more than an if/else, you might find a switch useful.
+        It 'is used to create multiple possible branches' {
+            $Folders = 4..9 | ForEach-Object {"$_" * $_}
+            switch ($Folders.Count) {
+                1 {
+                    # Switch statements assign the current value being tested to $_
+                    $Amount = 'one' + $_
+                }
+                5 {
+                    $Amount = 'five'
+                }
+                default {
+                    $Amount = "other $_"
+                }
+            }
+            $Amount | Should -Be '__'
+        }
+        It 'can also be used to conditionally assign values' {
 
         }
-        It "can also be used to conditionally assign values" {
+        It 'can go through multiple branches' {
 
         }
-        It "can go through multiple branches" {
+        It 'can stop after the first matching condition' {
 
         }
-        It "can stop after the first matching condition" {
+        It 'accepts wildcard conditions' {
 
         }
-        It "accepts wildcard conditions" {
+        It 'accepts regex conditions' {
 
         }
-        It "accepts regex conditions" {
-            
+        It 'allows use of conditional expressions' {
+
         }
     }
 }
