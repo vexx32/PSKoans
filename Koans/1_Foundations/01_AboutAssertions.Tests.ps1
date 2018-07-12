6350d09856ad15c53e45eeeab6c489bbfc6585ef
Joel <jfrancis@heritageflooring.com>
2018-07-12 11:47:04 -0400

    Remove more trailing whitespace

--- Koans/1_Foundations/01_AboutAssertions.Tests.ps1
+++ Koans/1_Foundations/01_AboutAssertions.Tests.ps1
@@ -1,20 +1,20 @@
 <#
     Getting Started
 
-    The PowerShell Koans are a set of exercises designed to get you familiar 
-    with PowerShell. By the time you're done, you'll have a basic 
+    The PowerShell Koans are a set of exercises designed to get you familiar
+    with PowerShell. By the time you're done, you'll have a basic
     understanding of the syntax of PoSH and learn a little more
     about scripting in general.
 
     Answering Problems
 
-    This is where the fun begins! Each koan contains an example designed 
-    to teach you a lesson about PowerShell. If you execute the program 
-    defined in this project, you will get a message that the koan below 
-    has failed. Your job is to fill in the blanks (the __, $__, FILL_ME_IN, 
-    or $FILL_ME_IN symbols) to make it pass. Once you make the change, 
-    re-run the program to make sure the koan passes, and continue on to the 
-    next failing koan. With each  passing koan, you'll learn more about 
+    This is where the fun begins! Each koan contains an example designed
+    to teach you a lesson about PowerShell. If you execute the program
+    defined in this project, you will get a message that the koan below
+    has failed. Your job is to fill in the blanks (the __, $__, FILL_ME_IN,
+    or $FILL_ME_IN symbols) to make it pass. Once you make the change,
+    re-run the program to make sure the koan passes, and continue on to the
+    next failing koan. With each  passing koan, you'll learn more about
     PowerShell, and add another weapon to your PowerShell scripting arsenal.
 #>
 Describe "Equality" {
@@ -33,7 +33,7 @@ Describe "Equality" {
     It "sets the expectations" {
         # Many roads converge, yet some paths are less clear.
         $ExpectedValue = 1 + 1
-        $ActualValue = __ 
+        $ActualValue = __
 
         $ExpectedValue -eq $ActualValue | Should -BeTrue
     }
