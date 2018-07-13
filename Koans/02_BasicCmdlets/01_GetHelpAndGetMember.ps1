c9edfbe2f81edbc1f5dc8fb195653d5431c18aab
Joel <jfrancis@heritageflooring.com>
2018-07-12 22:27:06 -0400

    Add intro to Get-Help and Get-Member

---
+++ Koans/02_BasicCmdlets/01_GetHelpAndGetMember.ps1
@@ -0,0 +1,19 @@
+<#
+    Get-Help
+
+    Get-Help is a built-in PowerShell cmdlet that is used to retrieve help data for
+    cmdlets and functions. It contains usage examples, parameter information,
+    and a significant amount of otherwise difficult to discover tidbits.
+
+    Get-Member
+
+    Get-Member is another built-in cmdlet that is invaluable in retrieving
+    information about any object in PowerShell. It can be used to inspect the type
+    name of an object, as well as all available methods and properties that can be
+    accessed for that object.
+
+    These cmdlets are quintessential discovery tools, and regular use of them will
+    vastly expedite any unfamiliar task in PowerShell. Combined with well-placed
+    Google searches, it is possible to learn a significant amount about native
+    PowerShell cmdlets and functions, and more advanced .NET classes, and methods.
+#>
