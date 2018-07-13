b73761f0dd37d9efc10f857d1273024aa8f38a82
Brandon Lundt <brandon.lundt@plexus.com>
2018-07-13 11:20:36 -0500

    Removed extra white space added by vscode

--- PSKoans.psm1
+++ PSKoans.psm1
@@ -133,8 +133,8 @@ function Write-MeditationPrompt {
 
 	if ($PSCmdlet.ParameterSetName -eq 'Greeting') {
 		Write-Host -ForegroundColor Cyan @"
-    Welcome, seeker of enlightenment.
-    Please wait a moment while we examine your karma...
+Welcome, seeker of enlightenment.
+Please wait a moment while we examine your karma...
 
 "@
 		return
@@ -142,48 +142,48 @@ function Write-MeditationPrompt {
 	Write-Host @Red @"
 {Describe "$DescribeName"} has damaged your karma.
 "@
-	Start-Sleep @SleepTime
-	Write-Host @Blue @"
+Start-Sleep @SleepTime
+Write-Host @Blue @"
 
-    You have not yet reached enlightenment.
+You have not yet reached enlightenment.
 
-    The answers you seek...
+The answers you seek...
 
 "@
 	Write-Host @Red @"
 $Expectation
 "@
-	Start-Sleep @SleepTime
-	Write-Host @Blue @"
+Start-Sleep @SleepTime
+Write-Host @Blue @"
 
-    Please meditate on the following code:
+Please meditate on the following code:
 
 "@
 	Write-Host @Red @"
 [It] $ItName
 $Meditation
 "@
-	Start-Sleep @SleepTime
-	Write-Host @Blue @"
+Start-Sleep @SleepTime
+Write-Host @Blue @"
 
-    $($Koan -replace "`n","`n    ")
+$($Koan -replace "`n","`n    ")
 
-    Your path thus far:
+Your path thus far:
 
 "@
-	$ProgressAmount = "$KoansPassed/$TotalKoans"
-	[int]$ProgressWidth = $host.UI.RawUI.WindowSize.Width * 0.8 - ($ProgressAmount.Length + 4)
-	$PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
+$ProgressAmount = "$KoansPassed/$TotalKoans"
+[int]$ProgressWidth = $host.UI.RawUI.WindowSize.Width * 0.8 - ($ProgressAmount.Length + 4)
+$PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
 
-	" [{0}{1}] {2}" -f @(
-		"$([char]0x25a0)" * $PortionDone
-		"$([char]0x2015)" * ($ProgressWidth - $PortionDone)
-		$ProgressAmount
-	) | Write-Host @Blue
+" [{0}{1}] {2}" -f @(
+    "$([char]0x25a0)" * $PortionDone
+    "$([char]0x2015)" * ($ProgressWidth - $PortionDone)
+    $ProgressAmount
+) | Write-Host @Blue
 
-	Write-Host @Blue @"
+Write-Host @Blue @"
 
-    You may run 'rake -Meditate' to begin your meditation.
+You may run 'rake -Meditate' to begin your meditation.
 
 "@
 }
