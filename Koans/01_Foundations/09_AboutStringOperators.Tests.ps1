0098f68e5be54644d142408dbda72366a8ebf925
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-15 13:45:04 -0400

    Finish standard comparisons

--- Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
+++ Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
@@ -20,18 +20,58 @@
 #>
 
 Describe 'String Comparison Operators' {
+    <#
+        String comparisons work a bit differently to other comparisons. Rather than comparing
+        the value of data, the strings are sorted, and the result of the sort determines
+        whether a given comparison returns $true.
 
+        In other words, 'ab' -lt 'bc'  is $true because the first string comes before the
+        second in a simple ascending sort). -eq and -ne work basically as expected.
+
+        Unless specified, all operators are case-insensitive. To make a given operator
+        case sensitive, prefix it with 'c' (e.g., -ceq, -creplace, -cmatch)
+    #>
     Context 'Equals and NotEquals' {
 
+        It 'tells you whether strings are the same' {
+            $String = 'this is a string.'
+            $OtherString = 'This is a string.'
+
+            $String -eq $OtherString | Should -Be __
+            # Watch out for case sensitive operators!
+            $String -ceq $OtherString | Should -Be __
+        }
+
+        It 'is useful for a straightforward comparison' {
+            $String = 'one more string!'
+            $OtherString = "ONE MORE STRING!"
+
+            $String -ne $OtherString | Should -Be __
+            $String -cne $OtherString | Should -Be __
+        }
     }
 
     Context 'GreaterThan and LessThan' {
 
+        It 'tells you where strings are in a sort' {
+            $String = 'my string'
+            $OtherString = 'your string'
+
+            $String -gt $OtherString | Should -Be __
+            $String -lt $OtherString | Should -Be __
+        }
     }
 }
 
 Describe 'String Array Operators' {
 
+    Context 'Split Operator' {
+
+    }
+
+    Context 'Join Operator' {
+
+    }
 }
 
 Describe 'Regex Operators' {
