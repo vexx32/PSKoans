54d3d4c98ab776d3d30d526090e7774bb478de43
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-15 13:06:19 -0400

    Complete introductory help

--- Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
+++ Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
@@ -1,7 +1,22 @@
 <#
     String Operators
 
-    -f, [ ], $( ), -join, -replace, -split, -match, -notmatch, -gt, -lt
+    While the standard comparison operators can be used with strings, there are also
+    several operators that work exclusively with strings.
+
+    In no particular order:
+
+    Operator            Name                        Purpose
+    --------            ----                        -------
+    -eq, -ne            Equal, Not Equal            Compare string
+    -gt, -lt            GreaterThan, LessThan       Compare string sort order
+    -f                  Format                      Insert and format variables/expressions
+    [ ]                 Index                       Access characters in string
+    $( )                Subexpression               Insert complex expressions into string
+    -join               Join                        Join string array into single string
+    -replace            Replace                     Regex: replace characters
+    -split              Split                       Regex: split string into array
+    -match, -notmatch   Match, Notmatch             Regex: compare string with regex expression
 #>
 
 Describe 'String Comparison Operators' {
