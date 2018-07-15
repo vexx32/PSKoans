933d23cea5a0ef45ea8bee6b533bacc6eebdabdc
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-15 02:56:26 -0400

    Begin laying out framework

--- Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
+++ Koans/01_Foundations/09_AboutStringOperators.Tests.ps1
@@ -1,3 +1,28 @@
-Describe 'String Operators' {
+<#
+    String Operators
+
+    -f, [ ], $( ), -join, -replace, -split, -match, -notmatch, -gt, -lt
+#>
+
+Describe 'String Comparison Operators' {
+
+    Context 'Equals and NotEquals' {
+
+    }
+
+    Context 'GreaterThan and LessThan' {
+
+    }
+}
+
+Describe 'String Array Operators' {
+
+}
+
+Describe 'Regex Operators' {
+
+}
+
+Describe 'Formatting Operators' {
 
 }
