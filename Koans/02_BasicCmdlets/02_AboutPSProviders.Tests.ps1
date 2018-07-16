dc21714734960d331c37c6f5b8b4dd9dfb8101e8
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-16 00:06:21 -0400

    Lay out framework for PSProviders

---
+++ Koans/02_BasicCmdlets/02_AboutPSProviders.Tests.ps1
@@ -0,0 +1,26 @@
+<#
+    PSProviders
+#>
+Describe 'Alias' {
+
+}
+
+Describe 'Environment' {
+
+}
+
+Describe 'FileSystem' {
+
+}
+
+Describe 'Function' {
+
+}
+
+Describe 'Variable' {
+
+}
+
+Describe 'Registry' -Tag 'Windows' {
+
+}
