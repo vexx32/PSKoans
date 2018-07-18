7a67b5480c8f517064c6871880632bf264bad29e
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-17 23:39:49 -0400

    Alias provider koans

--- Koans/02_BasicCmdlets/02_AboutPSProviders.Tests.ps1
+++ Koans/02_BasicCmdlets/02_AboutPSProviders.Tests.ps1
@@ -18,8 +18,32 @@
     On Windows, PowerShell also comes with a Registry provider, for interacting with the Windows
     registry.
 #>
-Describe 'Alias' {
+Describe 'Alias:' {
+    <#
+        Aliases are PowerShell command shortcuts. By querying the Alias: provider, you can get a
+        list of all command shortcuts in the current session. The available aliases may increase
+        when a new module is imported.
+    #>
+    It 'can be queried with cmdlets' {
+        $Aliases = Get-ChildItem 'Alias:'
 
+        $Aliases.Count | Should -Be __
+    }
+
+    It 'maps aliases to the full command' {
+        $Alias = '__'
+        $AliasObject = Get-Item "Alias:\$Alias" -ErrorAction SilentlyContinue
+
+        $AliasObject.Definition | Should -Be 'Set-Location'
+    }
+
+    It 'can be accessed with Get-Alias' {
+        $AliasObjects = Get-ChildItem 'Alias:'
+        $AliasObjects2 = Get-Alias
+
+        $AliasObjects2.Count | Should -Be __
+        $AliasObjects | Should -Be $AliasObjects2
+    }
 }
 
 Describe 'Environment' {
