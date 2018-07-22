2bcf10c71677ffddda1d4c3123787315fd113962
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-22 00:47:40 -0400

    Add link to PowerShell by Mistake

--- PSKoans.psm1
+++ PSKoans.psm1
@@ -88,6 +88,14 @@ function Get-Enlightenment {
                 }
                 Write-MeditationPrompt @Meditation
             }
+            else {
+                $Meditation = @{
+                    Complete    = $true
+                    KoansPassed = $KoansPassed
+                    TotalKoans  = $PesterTestCount
+                }
+                Write-MeditationPrompt @Meditation
+            }
         }
     }
 } # end function
@@ -97,7 +105,7 @@ function Get-Blank {
 	$null
 }
 function Write-MeditationPrompt {
-	<#
+    <#
 	.NOTES
 		Name: Write-MeditationPrompt
 		Author: vexx32
@@ -106,115 +114,153 @@ function Write-MeditationPrompt {
 	.DESCRIPTION
 		Provides a mechanism for Get-Enlightenment to write clean output.
 	#>
-	[CmdletBinding(DefaultParameterSetName = 'Meditation')]
-	param(
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNullOrEmpty()]
-		[string]
-		$DescribeName,
+    [CmdletBinding(DefaultParameterSetName = 'Meditation')]
+    param(
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $DescribeName,
 
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNullOrEmpty()]
-		[string]
-		$Expectation,
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Expectation,
 
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNullOrEmpty()]
-		[string]
-		$ItName,
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ItName,
 
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNullOrEmpty()]
-		[string]
-		$Meditation,
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Meditation,
 
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNull()]
-		[int]
-		$KoansPassed,
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [Parameter(Mandatory, ParameterSetName = 'Enlightened')]
+        [ValidateNotNull()]
+        [int]
+        $KoansPassed,
 
-		[Parameter(Mandatory, ParameterSetName = "Meditation")]
-		[ValidateNotNull()]
-		[int]
-		$TotalKoans,
+        [Parameter(Mandatory, ParameterSetName = "Meditation")]
+        [Parameter(Mandatory, ParameterSetName = 'Enlightened')]
+        [ValidateNotNull()]
+        [int]
+        $TotalKoans,
 
-		[Parameter(Mandatory, ParameterSetName = 'Greeting')]
-		[switch]
-		$Greeting
-	)
+        [Parameter(Mandatory, ParameterSetName = 'Greeting')]
+        [switch]
+        $Greeting,
 
-	$Red = @{ForegroundColor = "Red"}
-	$Blue = @{ForegroundColor = "Cyan"}
-	$Koan = $Script:ZenSayings | Get-Random
+        [Parameter(Mandatory, ParameterSetName = 'Enlightened')]
+        [switch]
+        $Complete
+    )
+
+    $Red = @{ForegroundColor = "Red"}
+    $Blue = @{ForegroundColor = "Cyan"}
+    $Koan = $Script:ZenSayings | Get-Random
     $SleepTime = @{Milliseconds = 50}
 
     #region Prompt Text
     $Prompts = @{
-        Welcome     = @"
+        Welcome        = @"
     Welcome, seeker of enlightenment.
     Please wait a moment while we examine your karma...
 
 "@
-        Describe    = @"
+        Describe       = @"
 {Describe "$DescribeName"} has damaged your karma.
 "@
-        TestFailed  = @"
+        TestFailed     = @"
 
     You have not yet reached enlightenment.
 
     The answers you seek...
 
 "@
-        Expectation = $Expectation
-        Meditate    = @"
+        Expectation    = $Expectation
+        Meditate       = @"
 
     Please meditate on the following code:
 
 "@
-        Subject     = @"
+        Subject        = @"
 [It] $ItName
 $Meditation
 "@
-        Wisdom      = @"
+        Wisdom         = @"
 
     $($Koan -replace "`n","`n    ")
 
     Your path thus far:
 
 "@
-        OpenFolder  = @"
+        OpenFolder     = @"
 
 You may run 'rake -Meditate' to begin your meditation.
 
 "@
+        Completed      = @"
+    Congratulations! You have taken the first steps towards enlightenment.
+
+    You cast your gaze back upon the path that you have walked:
+
+"@
+        BookSuggestion = @"
+
+    If you would like to further your studies in this manner, consider investing in
+    'PowerShell by Mistake' by Don Jones - https://leanpub.com/powershell-by-mistake
+"@
     }
     #endregion Prompt Text
 
-	if ($PSCmdlet.ParameterSetName -eq 'Greeting') {
-		Write-Host -ForegroundColor Cyan $Prompts['Welcome']
-        return
+    switch ($PSCmdlet.ParameterSetName) {
+        'Greeting' {
+            Write-Host -ForegroundColor Cyan $Prompts['Welcome']
+            break
+        }
+        'Meditation' {
+            Write-Host @Red $Prompts['Describe']
+            Start-Sleep @SleepTime
+            Write-Host @Blue $Prompts['TestFailed']
+            Write-Host @Red $Prompts['Expectation']
+            Start-Sleep @SleepTime
+            Write-Host @Blue $Prompts['Meditate']
+            Write-Host @Red $Prompts['Subject']
+            Start-Sleep @SleepTime
+            Write-Host @Blue $Prompts['Wisdom']
+
+            $ProgressAmount = "$KoansPassed/$TotalKoans"
+            [int]$ProgressWidth = $host.UI.RawUI.WindowSize.Width * 0.8 - ($ProgressAmount.Length + 4)
+            $PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
+
+            " [{0}{1}] {2}" -f @(
+                "$([char]0x25a0)" * $PortionDone
+                "$([char]0x2015)" * ($ProgressWidth - $PortionDone)
+                $ProgressAmount
+            ) | Write-Host @Blue
+
+            Write-Host @Blue $Prompts['OpenFolder']
+            break
+        }
+        'Enlightened' {
+            Write-Host @Blue $Prompts['Completed']
+
+            $ProgressAmount = "$KoansPassed/$TotalKoans"
+            [int]$ProgressWidth = $host.UI.RawUI.WindowSize.Width * 0.8 - ($ProgressAmount.Length + 4)
+            $PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
+
+            " [{0}{1}] {2}" -f @(
+                "$([char]0x25a0)" * $PortionDone
+                "$([char]0x2015)" * ($ProgressWidth - $PortionDone)
+                $ProgressAmount
+            ) | Write-Host @Blue
+
+            Write-Host @Blue $Prompts['BookSuggestion']
+            break
+        }
     }
-    Write-Host @Red $Prompts['Describe']
-    Start-Sleep @SleepTime
-    Write-Host @Blue $Prompts['TestFailed']
-	Write-Host @Red $Prompts['Expectation']
-    Start-Sleep @SleepTime
-    Write-Host @Blue $Prompts['Meditate']
-    Write-Host @Red $Prompts['Subject']
-    Start-Sleep @SleepTime
-    Write-Host @Blue $Prompts['Wisdom']
-
-	$ProgressAmount = "$KoansPassed/$TotalKoans"
-	[int]$ProgressWidth = $host.UI.RawUI.WindowSize.Width * 0.8 - ($ProgressAmount.Length + 4)
-	$PortionDone = ($KoansPassed / $TotalKoans) * $ProgressWidth
-
-	" [{0}{1}] {2}" -f @(
-		"$([char]0x25a0)" * $PortionDone
-		"$([char]0x2015)" * ($ProgressWidth - $PortionDone)
-		$ProgressAmount
-    ) | Write-Host @Blue
-
-	Write-Host @Blue $Prompts['OpenFolder']
 }
 function Initialize-KoanDirectory {
     <#
