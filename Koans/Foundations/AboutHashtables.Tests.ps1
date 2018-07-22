2a57d98c3012526b39db9a1b54a18840dfe12219
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-22 13:02:03 -0400

    Hashtable koans galore!

--- Koans/Foundations/AboutHashtables.Tests.ps1
+++ Koans/Foundations/AboutHashtables.Tests.ps1
@@ -40,6 +40,12 @@ Describe 'Hashtables' {
             $Hashtable['Spectrum'] | Should -Be '__'
         }
 
+        It 'can be built all in one line' {
+            $Hashtable = @{Name = 'Bob'; Species = 'Tardigrade'; Weakness = 'Phys'}
+
+            $Hashtable['Species'] | Should -Be '__'
+        }
+
         It 'can be built in pieces' {
             $Hashtable = @{}
             # By specifying a key, we can insert or overwrite values in the hashtable
@@ -64,5 +70,76 @@ Describe 'Hashtables' {
 
     Context 'Working with Hashtables' {
 
+        It 'is a reference type' {
+            $HashtableOne = @{
+                Name = "Jim"
+                Age  = 12
+            }
+
+            $HashtableTwo = $HashtableOne
+            $HashtableTwo['Age'] = 21
+
+            $HashtableOne['Age'] | Should -Be 12 # Right?
+            $HashtableTwo['Age'] | Should -Be __
+        }
+
+        It 'can be cloned' {
+            # Instead of passing a reference to the original, we can copy it!
+            $HashtableOne = @{
+                'Meal Type' = 'Dinner'
+                Calories    = 1287
+                Contents    = 'Carrots', 'Fish', 'Gherkin', 'Rice', 'Celery'
+            }
+            $HashtableTwo = $HashtableOne.Clone()
+            $HashtableTwo['Meal Type'] = 'Snack'
+            $HashtableTwo['Calories'] = 250
+            $HashtableTwo['Contents'] = 'Chips'
+
+            $HashtableOne['Meal Type'] | Should -Be '__'
+            $HashtableOne['Calories'] | Should -Be __
+
+            $HashtableTwo['Contents'] | Should -Be '__'
+        }
+
+        It 'allows you to retrieve a list of keys or values' {
+            $Hashtable = @{One = 1; Two = 2; Three = 3; Four = 4}
+
+            $Hashtable.Keys | Should -Be @('__', '__', '__', '__')
+            $Hashtable.Values | Should -Be @( , , , 4)
+        }
+
+        It 'allows you to remove keys' {
+            $Hashtable = @{
+                One   = 1
+                Two   = 2
+                Three = 3
+                Four  = 4
+            }
+
+            $Hashtable.Remove('One')
+
+            $Hashtable.Count | Should -Be __
+            $Hashtable.Keys | Should -Be @('__', '__', 'Four')
+            $Hashtable.Values | Should -Be @( , , 4)
+        }
+
+        It 'can check if keys or values are present in the hashtable' {
+            $Hashtable = @{
+                # Enter keys and values in this table to make the below tests pass
+            }
+
+            $Hashtable.ContainsKey('Apples') | Should -BeTrue
+            $Hashtable.ContainsValue('Fruit') | Should -BeTrue
+
+            $Hashtable['Oranges'] | Should -Be 'Fruit'
+            $Hashtable['Apples'] | Should -Not -Be $Hashtable['Oranges']
+        }
+
+        It 'will implicitly convert keys and lookup values' {
+            $Hashtable = @{0 = 'Zero'}
+
+            $Hashtable[0] | Should -Be '__'
+            $Hashtable['0'] | Should -Be '__'
+        }
     }
 }
