28ad3f1e14adf87ff069666a9daf7085572d3017
Joel <32407840+vexx32@users.noreply.github.com>
2018-07-23 12:29:21 -0400

    Build framework for Object cmdlet koans

---
+++ Koans/Cmdlets 1/AboutObjectCmdlets.Koans.ps1
@@ -0,0 +1,47 @@
+#Requires -Module PSKoans
+[Koan(203)]
+param()
+<#
+    *-Object Cmdlets
+
+    There are 9 standard '-Object' cmdlets in PowerShell, each designed to work with, well, objects! They are,
+    in no specific order: Compare-Object, ForEach-Object, Group-Object, Measure-Object, New-Object,
+    Select-Object, Sort-Object, Tee-Object, and Where-Object.
+#>
+Context 'Non-Pipeline Cmdlets' {
+
+    Describe 'New-Object' {
+
+    }
+
+    Describe 'Compare-Object' {
+
+    }
+}
+
+Context 'Pipeline Cmdlets' {
+
+    Describe 'Select-Object' {
+
+    }
+
+    Describe 'Where-Object' {
+
+    }
+
+    Describe 'ForEach-Object' {
+
+    }
+
+    Describe 'Tee-Object' {
+
+    }
+
+    Describe 'Group-Object' {
+
+    }
+
+    Describe 'Sort-Object' {
+
+    }
+}
