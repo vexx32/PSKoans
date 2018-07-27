66a6978243673982888321022d494337abbea797
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-07-26 22:01:39 -0400

    Give control of when we deploy to our script rather than appveyor.yml

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -3,6 +3,10 @@ param (
     $Task = 'Default'
 )
 
+if ($env:APPVEYOR_REPO_BRANCH -eq 'master' -and -not $env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    $Task = 'Deploy'
+}
+
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap > $null
 
