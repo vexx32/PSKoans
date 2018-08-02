533a1856dfaadfd54ccf6602d2395395ff42e793
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-01 23:09:44 -0400

    Add Compare-Object koans

--- PSKoans/Koans/Cmdlets 1/AboutObjectCmdlets.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutObjectCmdlets.Koans.ps1
@@ -1,12 +1,12 @@
 #Requires -Module PSKoans
-[Koan(203)]
+[Koan(204)]
 param()
 <#
     *-Object Cmdlets
 
-    There are 9 standard '-Object' cmdlets in PowerShell, each designed to work with, well, objects! They are,
-    in no specific order: Compare-Object, ForEach-Object, Group-Object, Measure-Object, New-Object,
-    Select-Object, Sort-Object, Tee-Object, and Where-Object.
+    There are 9 standard '-Object' cmdlets in PowerShell, each designed to work with, well,
+    objects! They are, in no specific order: Compare-Object, ForEach-Object, Group-Object,
+    Measure-Object, New-Object, Select-Object, Sort-Object, Tee-Object, and Where-Object.
 #>
 Context 'Non-Pipeline Cmdlets' {
 
@@ -33,7 +33,31 @@ Context 'Non-Pipeline Cmdlets' {
     }
 
     Describe 'Compare-Object' {
+        <#
+            Compare-Object is primarily for comparing collections of objects rather than individual
+            items, and is usually used to display data comparisons to the user.
+        #>
+        It 'compares collections of objects' {
+            $Comparison = @{
+                ReferenceObject  = 'this', 'is', 'the', 'reference'
+                DifferenceObject = 'these', 'is', 'the', 'difference'
+            }
+            $CompareData = Compare-Object @Comparison
+            $CompareData[0].InputObject | Should -Be '__'
+            $CompareData[0].SideIndicator | Should -Be '__'
+            $CompareData[0] | Should -BeOfType __
+        }
 
+        It 'can display items that occur in both sets' {
+            # By default, items that are the same in both collections are hidden.
+            $Comparison = @{
+                ReferenceObject  = 'this', 'is', 'the', 'reference'
+                DifferenceObject = 'these', 'is', 'the', 'difference'
+            }
+            $CompareData = Compare-Object @Comparison -IncludeEqual
+            $CompareData.SideIndicator -contains '==' | Should -BeTrue
+            $CompareData.Where{$_.InputObject -eq 'the'}.SideIndicator | Should -Be '__'
+        }
     }
 }
 
