bdd193f9ffe531e294aa81cfe7460aa1fd9e0186
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-02 23:35:29 -0400

    Remove meaningless context distinction

--- PSKoans/Koans/Cmdlets 1/AboutObjectCmdlets.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutObjectCmdlets.Koans.ps1
@@ -8,102 +8,96 @@ param()
     objects! They are, in no specific order: Compare-Object, ForEach-Object, Group-Object,
     Measure-Object, New-Object, Select-Object, Sort-Object, Tee-Object, and Where-Object.
 #>
-Context 'Non-Pipeline Cmdlets' {
+Describe 'New-Object' {
 
-    Describe 'New-Object' {
+    It 'can create objects of a specified type' {
+        $Object = New-Object -TypeName 'string'
+        $String = 'string'
 
-        It 'can create objects of a specified type' {
-            $Object = New-Object -TypeName 'string'
-            $String = 'string'
-
-            $String | Should -BeOfType string
-            $Object | Should -BeOfType __
-        }
-
-        It 'can create objects of any available type' {
-            $Object = New-Object -TypeName 'System.Collections.ArrayList'
-            $Object | Should -BeOfType __
-        }
-
-        It 'can accept arguments for the constructor' {
-            $Object = New-Object 'string' -ArgumentList ([char[]]@('b','a','n','a','n','a'), 0, 3)
-            '__' | Should -Be $Object
-            $Object | Should -BeOfType string
-        }
+        $String | Should -BeOfType string
+        $Object | Should -BeOfType __
     }
 
-    Describe 'Compare-Object' {
-        <#
-            Compare-Object is primarily for comparing collections of objects rather than individual
-            items, and is usually used to display data comparisons to the user.
-        #>
-        It 'compares collections of objects' {
-            $Comparison = @{
-                ReferenceObject  = 'this', 'is', 'the', 'reference'
-                DifferenceObject = 'these', 'is', 'the', 'difference'
-            }
-            $CompareData = Compare-Object @Comparison
-            $CompareData[0].InputObject | Should -Be '__'
-            $CompareData[0].SideIndicator | Should -Be '__'
-            $CompareData[0] | Should -BeOfType __
-        }
+    It 'can create objects of any available type' {
+        $Object = New-Object -TypeName 'System.Collections.ArrayList'
+        $Object | Should -BeOfType __
+    }
 
-        It 'can display items that occur in both sets' {
-            # By default, items that are the same in both collections are hidden.
-            $Comparison = @{
-                ReferenceObject  = 'this', 'is', 'the', 'reference'
-                DifferenceObject = 'these', 'is', 'the', 'difference'
-            }
-            $CompareData = Compare-Object @Comparison -IncludeEqual
-            $CompareData.SideIndicator -contains '==' | Should -BeTrue
-            $CompareData.Where{$_.InputObject -eq 'the'}.SideIndicator | Should -Be '__'
-        }
+    It 'can accept arguments for the constructor' {
+        $Object = New-Object 'string' -ArgumentList ([char[]]@('b','a','n','a','n','a'), 0, 3)
+        '__' | Should -Be $Object
+        $Object | Should -BeOfType string
     }
 }
 
-Context 'Pipeline Cmdlets' {
-
-    Describe 'Select-Object' {
-        <#
-            Select-Object is a utility cmdlet that is used to 'trim' objects down to just
-            the 'selected' properties. It is particularly useful to get custom displays
-            of data, and is capable of adding new properties as well.
-        #>
-
-        It 'selects specific properties of an object' {
-
+Describe 'Compare-Object' {
+    <#
+        Compare-Object is primarily for comparing collections of objects rather than individual
+        items, and is usually used to display data comparisons to the user.
+    #>
+    It 'compares collections of objects' {
+        $Comparison = @{
+            ReferenceObject  = 'this', 'is', 'the', 'reference'
+            DifferenceObject = 'these', 'is', 'the', 'difference'
         }
+        $CompareData = Compare-Object @Comparison
+        $CompareData[0].InputObject | Should -Be '__'
+        $CompareData[0].SideIndicator | Should -Be '__'
+        $CompareData[0] | Should -BeOfType __
+    }
 
-        It 'changes the object type' {
-
+    It 'can display items that occur in both sets' {
+        # By default, items that are the same in both collections are hidden.
+        $Comparison = @{
+            ReferenceObject  = 'this', 'is', 'the', 'reference'
+            DifferenceObject = 'these', 'is', 'the', 'difference'
         }
-
-        It 'can exclude properties' {
-
-        }
-
-        It 'can retrieve just the contents or value of a property' {
-
-        }
+        $CompareData = Compare-Object @Comparison -IncludeEqual
+        $CompareData.SideIndicator -contains '==' | Should -BeTrue
+        $CompareData.Where{$_.InputObject -eq 'the'}.SideIndicator | Should -Be '__'
     }
+}
 
-    Describe 'Where-Object' {
+Describe 'Select-Object' {
+    <#
+        Select-Object is a utility cmdlet that is used to 'trim' objects down to just
+        the 'selected' properties. It is particularly useful to get custom displays
+        of data, and is capable of adding new properties as well.
+    #>
 
-    }
-
-    Describe 'ForEach-Object' {
-
-    }
-
-    Describe 'Tee-Object' {
+    It 'selects specific properties of an object' {
 
     }
 
-    Describe 'Group-Object' {
+    It 'changes the object type' {
 
     }
 
-    Describe 'Sort-Object' {
+    It 'can exclude properties' {
 
     }
+
+    It 'can retrieve just the contents or value of a property' {
+
+    }
+}
+
+Describe 'Where-Object' {
+
+}
+
+Describe 'ForEach-Object' {
+
+}
+
+Describe 'Tee-Object' {
+
+}
+
+Describe 'Group-Object' {
+
+}
+
+Describe 'Sort-Object' {
+
 }
