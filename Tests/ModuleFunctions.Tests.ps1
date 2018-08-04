337ac9e9caa0ad4e6a65d2397feb6a5cd807ee39
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-04 16:22:44 -0400

    Add allowance for getting module path locally
    This doesn't work in Appveyor for LITERALLY NO REASON
    
    So there we're using AV's env variables.

--- Tests/ModuleFunctions.Tests.ps1
+++ Tests/ModuleFunctions.Tests.ps1
@@ -2,6 +2,10 @@ $ProjectRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'Tests
 $ModuleFolder = "$ProjectRoot\PSKoans"
 Import-Module $ModuleFolder -Prefix "TST"
 
+if (-not $env:APPVEYOR_BUILD_FOLDER) {
+    $env:APPVEYOR_BUILD_FOLDER = (Get-Module -Name 'PSKoans').ModuleBase
+}
+
 Describe 'Get-Blank' {
 
     It 'should not produce output' {
