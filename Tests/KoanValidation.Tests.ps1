67ade87a8d2330deff667d569f9c3d2593c4d70e
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-03 23:51:04 -0400

    Add tests to validate there are no koans with syntax errors

---
+++ Tests/KoanValidation.Tests.ps1
@@ -0,0 +1,20 @@
+$ProjectRoot = Resolve-Path "$PSScriptRoot\.."
+$KoanFolder = $ProjectRoot | Join-Path -ChildPath 'PSKoans' | Join-Path 'Koans'
+
+Describe "Koan Assessment" {
+
+    $Scripts = Get-ChildItem -Path $KoanFolder -Recurse -Filter '*.Koans.ps1'
+
+    # TestCases are splatted to the script so we need hashtables
+    $TestCases = $Scripts | ForEach-Object { @{File = $_} }
+    It "<File> koans should be valid powershell" -TestCases $TestCases {
+        param($File)
+
+        $File.FullName | Should -Exist
+
+        $FileContents = Get-Content -Path $File.FullName -ErrorAction Stop
+        $Errors = $null
+        [System.Management.Automation.PSParser]::Tokenize($FileContents, [ref]$Errors) > $null
+        $Errors.Count | Should -Be 0
+    }
+}
