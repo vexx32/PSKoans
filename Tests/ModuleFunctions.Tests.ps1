94e046fe89068f280a2e969d0593e6f53e0be0bf
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-04 13:09:05 -0400

    Write-MeditationPrompt tests
    Start Get-Enlightenment tests

--- Tests/ModuleFunctions.Tests.ps1
+++ Tests/ModuleFunctions.Tests.ps1
@@ -9,6 +9,14 @@ Describe 'Get-Blank' {
     }
 }
 
+Describe 'Get-Enlightenment' {
+    BeforeAll {
+        Mock Initialize-KoanDirectory
+        Mock Start-Process
+        Mock Invoke-Item
+    }
+}
+
 InModuleScope 'PSKoans' {
     Describe 'Initialize-KoanDirectory with Mocked Commands' -Tags 'Mock' {
         BeforeAll {
@@ -81,34 +89,38 @@ InModuleScope 'PSKoans' {
                 Test-Path -Path "$env:PSKoans_Folder\$File" | Should -BeTrue
 
                 $CopiedFile = Get-Item -Path "$env:PSKoans_Folder\$File"
-                $OriginalFile = Get-Item -Path "$ModuleFolder\$File"
+                $OriginalFile = Get-Item -Path "$ModuleFolder\Koans\$File"
                 $OriginalHash = Get-FileHash -Path $CopiedFile.FullName
                 $CopiedHash = Get-FileHash -Path $OriginalFile.FullName
 
                 # Verify the files are the same
                 $CopiedFile.Length | Should -Be $OriginalFile.Length
-                $CopiedHash | Should -Be $OriginalHash
+                $CopiedHash.Hash | Should -Be $OriginalHash.Hash
             }
         }
 
         Context 'Koan Folder Does Not Exist' {
-
-            $TestCases = Get-ChildItem -Path "$ModuleFolder\Koans" -Recurse -File -Filter '*.Koans.ps1' | ForEach-Object {
-                @{File = $_.FullName -replace '.+\\Koans\\'}
+            BeforeAll {
+                $TestCases = Get-ChildItem -Path "$ModuleFolder\Koans" -Recurse -File -Filter '*.Koans.ps1' | ForEach-Object {
+                    @{File = $_.FullName -replace '.+\\Koans\\'}
+                }
             }
+
+            Initialize-KoanDirectory -Confirm:$false
+
             It 'should copy <File> to the Koans folder' -TestCases $TestCases {
                 param($File)
 
                 Test-Path -Path "$env:PSKoans_Folder\$File" | Should -BeTrue
 
                 $CopiedFile = Get-Item -Path "$env:PSKoans_Folder\$File"
-                $OriginalFile = Get-Item -Path "$ModuleFolder\$File"
+                $OriginalFile = Get-Item -Path "$ModuleFolder\Koans\$File"
                 $OriginalHash = Get-FileHash -Path $CopiedFile.FullName
                 $CopiedHash = Get-FileHash -Path $OriginalFile.FullName
 
                 # Verify the files are the same
                 $CopiedFile.Length | Should -Be $OriginalFile.Length
-                $CopiedHash | Should -Be $OriginalHash
+                $CopiedHash.Hash | Should -Be $OriginalHash.Hash
             }
         }
 
@@ -118,5 +130,58 @@ InModuleScope 'PSKoans' {
             }
         }
     }
-}
 
+    Describe 'Write-MeditationPrompt' {
+        BeforeAll {
+            Mock Write-Host
+            Mock Start-Sleep
+            Mock Import-CliXml {"Test"}
+        }
+
+        Context 'Greeting Prompt' {
+
+            It 'should only write a single string to host' {
+                Write-MeditationPrompt -Greeting
+
+                Assert-MockCalled Write-Host -Times 1
+            }
+        }
+
+        Context 'Standard Prompt' {
+            BeforeAll {
+                $Meditation = @{
+                    DescribeName = "TestDescribe"
+                    Expectation  = "TestExpectation"
+                    ItName       = "TestIt"
+                    Meditation   = "TestMeditation"
+                    KoansPassed  = 0
+                    TotalKoans   = 100
+                }
+            }
+
+            It 'should display the entire prompt' {
+                Write-MeditationPrompt @Meditation
+
+                Assert-MockCalled Import-Clixml -Times 1
+                Assert-MockCalled Write-Host -Times 8
+                Assert-MockCalled Start-Sleep -Times 3
+            }
+        }
+
+        Context 'Enlightened Prompt' {
+            BeforeAll {
+                $Meditation = @{
+                    Complete    = $true
+                    KoansPassed = 100
+                    TotalKoans  = 100
+                }
+            }
+
+            It 'should display the enlightened prompt' {
+                Write-MeditationPrompt @Meditation
+
+                Assert-MockCalled Write-Host -Times 3
+            }
+        }
+    }
+}
