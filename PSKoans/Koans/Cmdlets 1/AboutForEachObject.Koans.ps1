8796cb09539a3304e532bfc2c0c13dfc6e59f0dc
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-05 11:35:06 -0400

    Add intro text for ForEach-Object koans

--- PSKoans/Koans/Cmdlets 1/AboutForEachObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutForEachObject.Koans.ps1
@@ -4,7 +4,11 @@ param()
 <#
     ForEach-Object
 
+    As the name implies, ForEach-Object acts like a loop, similar to the native
+    foreach keyword.
 
+    It can be used at any point in a pipeline context to perform custom actions
+    not otherwise performed by existing cmdlets.
 #>
 Describe 'ForEach-Object' {
 
