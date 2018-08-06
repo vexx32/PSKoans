5cceb3c2e2d4e763a997b37eb099b4e1251b1049
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-05 22:09:13 -0400

    Add intro for Tee-Object

--- PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
@@ -4,7 +4,9 @@ param()
 <#
     Tee-Object
 
-
+    Tee-Object is a utility cmdlet that splits the path of pipeline
+    objects. It directly passes through all objects that it receives,
+    as well as storing a copy of the items either in a file or variable.
 #>
 Describe 'Tee-Object' {
 
