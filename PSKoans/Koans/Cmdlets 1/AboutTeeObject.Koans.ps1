2f22819e5e934eab37c05b1524895698da3be21e
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-05 22:43:04 -0400

    Add Tee-Object koans

--- PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
@@ -10,4 +10,20 @@ param()
 #>
 Describe 'Tee-Object' {
 
+    It 'passes the object on through the pipeline' {
+        1..10 | Tee-Object -Variable n | Should -Be __
+    }
+
+    It 'can store the object(s) into a variable' {
+        # Note the variable name is passed as a string, without the $!
+        'alpha', 'beta', 'gamma' | Tee-Object -Variable 'Numbers' | Should -Be $Numbers
+        $Numbers[1] | Should -Be '__'
+    }
+
+    It 'can also store the object(s) into a file' {
+        $File = New-TemporaryFile
+
+        1..5 | ForEach-Object {"{0:N2}" -f (1 / $_)} | Tee-Object -FilePath $File.FullName | Should -Be __
+        $File | Get-Content | Should -Be __
+    }
 }
