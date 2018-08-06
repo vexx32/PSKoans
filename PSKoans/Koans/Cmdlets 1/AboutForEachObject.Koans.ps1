eed6353eaa418bc3bc009b1d0b4d1f84b9040e41
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-05 23:00:00 -0400

    Add ForEach-Object koans

--- PSKoans/Koans/Cmdlets 1/AboutForEachObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutForEachObject.Koans.ps1
@@ -9,7 +9,33 @@ param()
 
     It can be used at any point in a pipeline context to perform custom actions
     not otherwise performed by existing cmdlets.
+
+    ForEach-Object optionally takes parameters for Begin and End script blocks,
+    allowing it to become a sort of impromptu advanced function of sorts.
 #>
 Describe 'ForEach-Object' {
 
+    It 'is similar to foreach' {
+        $ForEachLoop = foreach ($Number in 1..10) {
+            "The number is $Number!"
+        }
+
+        $ForEachObject = 1..10 | ForEach-Object {
+            "__"
+        }
+
+        $ForEachLoop | Should -Be $ForEachObject
+    }
+
+    It 'takes Begin and End script blocks' {
+        $Output = 1..5 | ForEach-Object -Begin {'__'} -Process {$_} -End {'END'}
+
+        $Output[0] | Should -Be 'BACON'
+        $Output[-1] | Should -Be '__'
+    }
+
+    It 'can iterate over only a specific property of the objects' {
+        $Strings = 'hello', 'goodbye', 'what', 'what', 'you first'
+        $Strings | ForEach-Object -MemberName __ | Should -Be @(5, 7, 4, 4, 9)
+    }
 }
