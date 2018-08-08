6b2103b8298f236d1b3edf018faaa62d28ca9669
Joel <32407840+vexx32@users.noreply.github.com>
2018-08-08 12:42:55 -0400

    Add intro to modules

--- PSKoans/Koans/Cmdlets 1/AboutModules.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutModules.Koans.ps1
@@ -1,3 +1,21 @@
 #Requires -Module PSKoans
 [Koan(Position = 212)]
 param()
+<#
+    Modules
+
+    PowerShell is built from various types of modules, which come in three flavours:
+        - Script modules
+        - Manifest modules
+        - Binary modules
+
+    Manifest modules are the least common, consisting only of a single .psd1 file;
+    their primary code is often stored in a core library of PowerShell and is not
+    considered part of 'the module' itself, merely exposed by it.
+
+    Binary modules are compiled from C# using the PowerShell libraries and tend to
+    be in .dll formats with other files providing metadata.
+
+    Script modules commonly consist of .psm1, .psd1, .ps1 and other files, and are
+    one of the more common module types.
+#>
