d3793237ce1632b1647bfd1f418f7ffdc4f830f5
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-08-11 21:27:34 -0400

    Add verbose logging Add comment help

--- PSKoans/PSKoans.psm1
+++ PSKoans/PSKoans.psm1
@@ -1,11 +1,31 @@
+<#
+.SYNOPSIS
+    Provides a library of editable Pester tests to assist learning PowerShell.
+.DESCRIPTION
+    Provides a simplified interface with Pester tests that describe core PowerShell
+    functions, language features, and techniques, in order to facilitate quickly
+    learning the PowerShell language.
+.EXAMPLE
+    PS> Measure-Karma
+
+    Execute the recorded tests to check your progress.
+.NOTES
+    Author: Joel Sallow
+#>
 Get-ChildItem "$PSScriptRoot/Public", "$PSScriptRoot/Private" | ForEach-Object {
+    Write-Verbose "Importing functions from file: [$($_.Name)]"
     . $_.FullName
 }
+
+Write-Verbose 'Importing class-based types'
 . "$PSScriptRoot/ExportedTypes.ps1"
 
 $env:PSKoans_Folder = $Home | Join-Path -ChildPath 'PSKoans'
+Write-Verbose "Koans folder set to $env:PSKoans_Folder"
+
 $script:ModuleFolder = $PSScriptRoot
 
 if (-not (Test-Path -Path $env:PSKoans_Folder)) {
+    Write-Verbose 'Koans folder does not exist; populating the folder'
 	Initialize-KoanDirectory -Confirm:$false
 }
