1152368f2156c0d753a6f9a0e9eac3073ef63fdb
Joel <32407840+vexx32@users.noreply.github.com>
2018-08-13 13:32:23 -0400

    style fixes

--- TESTFILE-ASTPARSER.ps1
+++ TESTFILE-ASTPARSER.ps1
@@ -1,11 +1,13 @@
 using namespace System.Management.Automation.Language
 
 $SyntaxTree = (Get-Command "Z:\Scripts\PSKoans\Tests\KoanValidation.Tests.ps1").ScriptBlock.Ast
-$ItCommands = $SyntaxTree.FindAll( {
-    param ($AstItem)
+$ItCommands = $SyntaxTree.FindAll(
+    {
+        param ($AstItem)
 
-    $AstItem -is [CommandAst] -and $AstItem.GetCommandName() -eq 'It'
-}, $true )
+        $AstItem -is [CommandAst] -and $AstItem.GetCommandName() -eq 'It'
+    }, $true
+)
 
 $Parameter = $ItCommands.CommandElements | Where-Object {
     $_ -is [CommandParameterAst] -and
