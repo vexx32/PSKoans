b7ee594f871f52129abc34e5c6505071cc9f654b
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2018-08-29 23:53:48 +0200

    Added advice basics

---
+++ PSKoans/Public/Invoke-Advice.ps1
@@ -0,0 +1,74 @@
+﻿function Invoke-Advice
+{
+<#
+	.SYNOPSIS
+		Prints a piece of advice to the screen.
+	
+	.DESCRIPTION
+		Prints a piece of advice to the screen.
+	
+		Use Register-Advice to have a random piece of advice shown on each console start
+	
+	.PARAMETER Name
+		The name of the specific advice to display
+	
+	.EXAMPLE
+		PS C:\> Invoke-Advice
+	
+		Print a random piece of advice to the screen.
+#>
+	[CmdletBinding()]
+	Param (
+		[string]
+		$Name = "*"
+	)
+	
+	begin
+	{
+		$adviceFolder = Join-Path (Join-Path $script:ModuleFolder 'Data') 'Advice'
+		
+		function Write-Line
+		{
+			[CmdletBinding()]
+			param (
+				[string]
+				$Line
+			)
+			
+			$width = $host.UI.RawUI.WindowSize.Width
+			
+			# Ugly mode since width either not detectable or too small to bother
+			if ($width -lt 20) { Write-Host "    $Line" }
+			else
+			{
+				$remainingLine = $Line.TrimEnd()
+				$lines = @()
+				
+				while ($remainingLine.Length -gt ($width - 4))
+				{
+					$subString = $remainingLine.Substring(0, ($width - 4))
+					$end = ($subString -split " |-")[-1]
+					$lines += $subString.Substring(0, ($subString.Length - $end.Length)).TrimEnd()
+					$remainingLine = $remainingLine.Substring(($subString.Length - $end.Length))
+				}
+				$lines += $remainingLine
+				
+				foreach ($lineItem in $lines)
+				{
+					Write-Host "    $lineItem"
+				}
+			}
+		}
+	}
+	process
+	{
+		$adviceItem = Get-ChildItem $adviceFolder -Recurse | Where-Object PSIsContainer -EQ $false | Where-Object BaseName -Like $Name | Get-Random
+		Write-Host ""
+		Write-Host "  Advice of the session:"
+		foreach ($line in (Get-Content $adviceItem.FullName))
+		{
+			Write-Line $line
+		}
+		Write-Host ""
+	}
+}
---
+++ PSKoans/Public/Register-Advice.ps1
@@ -0,0 +1,39 @@
+﻿function Register-Advice
+{
+<#
+	.SYNOPSIS
+		Causes powershell to write a random piece of advice on each start.
+	
+	.DESCRIPTION
+		Causes powershell to write a random piece of advice on each start.
+		This is done by creating / modifying the powershell profile.
+	
+	.EXAMPLE
+		PS C:\> Register-Advice
+	
+		Causes powershell to write a random piece of advice on each start.
+#>
+	[CmdletBinding()]
+	Param (
+		
+	)
+	
+	process
+	{
+		if (-not (Test-Path $profile))
+		{
+			$folder = Split-Path $PROFILE
+			if (-not (Test-Path $folder))
+			{
+				$null = New-Item $folder -ItemType Directory -Force
+			}
+			Set-Content -Path $PROFILE -Value 'Invoke-Advice'
+		}
+		elseif (-not ((Get-Content $PROFILE) -match "Invoke-Advice"))
+		{
+			[string[]]$lines = Get-Content $PROFILE
+			$lines += 'Invoke-Advice'
+			$lines | Set-Content $PROFILE
+		}
+	}
+}
