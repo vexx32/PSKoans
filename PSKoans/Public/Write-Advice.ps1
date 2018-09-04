bec58a80563ddfe5d1827174591bab48d7135356
Joel <32407840+vexx32@users.noreply.github.com>
2018-09-04 09:47:56 -0400

    Rename file for more appropriate verb

--- PSKoans/Public/Write-Advice.ps1
+++ PSKoans/Public/Write-Advice.ps1
@@ -1,32 +1,28 @@
-﻿function Invoke-Advice
-{
-<#
-	.SYNOPSIS
-		Prints a piece of advice to the screen.
-	
-	.DESCRIPTION
-		Prints a piece of advice to the screen.
-	
-		Use Register-Advice to have a random piece of advice shown on each console start
-	
-	.PARAMETER Name
-		The name of the specific advice to display
-	
-	.EXAMPLE
-		PS C:\> Invoke-Advice
-	
-		Print a random piece of advice to the screen.
-#>
+﻿function Write-Advice {
+	<#
+		.SYNOPSIS
+			Prints a piece of advice to the screen.
+		.DESCRIPTION
+			Prints a piece of advice to the screen.
+
+			Use Register-Advice to have a random piece of advice shown on each console start
+		.PARAMETER Name
+			The name of the specific advice to display
+		.EXAMPLE
+			PS C:\> Invoke-Advice
+			Print a random piece of advice to the screen.
+	#>
 	[CmdletBinding()]
-	Param (
+	param(
+		[Parameter(Position = 0)]
 		[string]
 		$Name = "*"
 	)
-	
+
 	begin
 	{
-		$adviceFolder = Join-Path (Join-Path $script:ModuleFolder 'Data') 'Advice'
-		
+		$adviceFolder = Join-Path $script:ModuleFolder 'Data/Advice'
+
 		function Write-Line
 		{
 			[CmdletBinding()]
@@ -34,16 +30,16 @@
 				[string]
 				$Line
 			)
-			
+
 			$width = $host.UI.RawUI.WindowSize.Width
-			
+
 			# Ugly mode since width either not detectable or too small to bother
 			if ($width -lt 20) { Write-Host "    $Line" }
 			else
 			{
 				$remainingLine = $Line.TrimEnd()
 				$lines = @()
-				
+
 				while ($remainingLine.Length -gt ($width - 4))
 				{
 					$subString = $remainingLine.Substring(0, ($width - 4))
@@ -52,7 +48,7 @@
 					$remainingLine = $remainingLine.Substring(($subString.Length - $end.Length))
 				}
 				$lines += $remainingLine
-				
+
 				foreach ($lineItem in $lines)
 				{
 					Write-Host "    $lineItem"
