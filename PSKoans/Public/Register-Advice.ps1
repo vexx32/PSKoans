fc4a405dd99eac3553ac70f53cd6d915ab070622
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-09-13 21:04:08 -0400

    Fix & tidy Register-Advice

--- PSKoans/Public/Register-Advice.ps1
+++ PSKoans/Public/Register-Advice.ps1
@@ -1,37 +1,39 @@
-﻿function Register-Advice
-{
-<#
+﻿function Register-Advice {
+    <#
 	.SYNOPSIS
 		Causes powershell to write a random piece of advice on each start.
-	
+
 	.DESCRIPTION
 		Causes powershell to write a random piece of advice on each start.
 		This is done by creating / modifying the powershell profile.
-	
+
 	.EXAMPLE
 		PS C:\> Register-Advice
-	
+
 		Causes powershell to write a random piece of advice on each start.
 #>
-	[CmdletBinding()]
-	Param (
-		
-	)
-	
-	process
-	{
-		if (-not (Test-Path $profile))
-		{
-			$folder = Split-Path $PROFILE
-			if (-not (Test-Path $folder))
-			{
-				$null = New-Item $folder -ItemType Directory -Force
-			}
-			Set-Content -Path $PROFILE -Value 'Invoke-Advice'
-		}
-		elseif (-not ((Get-Content $PROFILE) -match "Invoke-Advice"))
-		{
-			'Invoke-Advice' | Add-Content $PROFILE
-		}
-	}
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+    param(
+        [Parameter(Position = 0)]
+        [ValidateSet('AllUsersAllHosts', 'AllUsersCurrentHost', 'CurrentUserAllHosts', 'CurrentUserCurrentHost')]
+        [string]
+        $TargetProfile = 'CurrentUserCurrentHost'
+    )
+
+	$ProfilePath = $Profile.$TargetProfile
+
+    if ($PSCmdlet.ShouldProcess("$TargetProfile PowerShell profile", 'Register Get-Advice')) {
+        $ProfileFolder = Split-Path -Path $ProfilePath
+
+        if (-not (Test-Path $ProfileFolder)) {
+            New-Item $ProfileFolder -ItemType Directory -Force > $null
+        }
+
+        if (-not (Test-Path $ProfilePath)) {
+            Set-Content -Path $ProfilePath -Value 'Get-Advice'
+        }
+        elseif ($ProfilePath | Select-String -Pattern 'Get-Advice' -Quiet) {
+            'Get-Advice' | Add-Content $ProfilePath
+        }
+    }
 }
