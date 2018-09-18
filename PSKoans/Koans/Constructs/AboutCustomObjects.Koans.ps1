a720027cb70a5e3a9f55e1b0f1dfc3f53cee2c3e
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-09-17 23:31:57 -0400

    Add some creation methods

--- PSKoans/Koans/Constructs/AboutCustomObjects.Koans.ps1
+++ PSKoans/Koans/Constructs/AboutCustomObjects.Koans.ps1
@@ -19,19 +19,33 @@ param()
 #>
 Describe '[PSCustomObject]' {
 
+    It 'can be built from a hashtable' {
+        <#
+            This behaves like a cast, but it's actually a bit different. A directly-cast hashtable
+            preserves the order of the elements, unlike regular hashtables, thanks to some parser
+            magic.
+        #>
+        $Object = [PSCustomObject]@{
+            Property = 'Value'
+        }
+        $Object | Should -BeOfType PSCustomObject
+    }
+
+    It 'can be built by trimming objects down' {
+        $Object = Get-ChildItem -Path $Home | Select-Object -First 1 -Property Name, Parent
+        $Object | Should -BeOfType PSCustomObject
+        __ | Should -Be $Object.Parent
+    }
+
     It 'can have arbitrary properties' {
 
     }
 
-    It 'can be built in multiple ways' {
+    It 'can be added to' {
 
     }
 
     It 'can have ScriptProperties' {
-
-    }
-
-    It 'can be used to organise data' {
 
     }
 }
