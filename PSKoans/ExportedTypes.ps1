6dd087159dcce6b22c5667d23167fcacad0d0b9a
romero126 <romero126@gmail.com>
2018-10-05 15:07:03 -0700

    Updated Koan Attribute to provide Help Actions.

--- PSKoans/ExportedTypes.ps1
+++ PSKoans/ExportedTypes.ps1
@@ -3,12 +3,24 @@ class __ : FILL_ME_IN {}
 
 class KoanAttribute : System.Attribute {
     [uint32] $Position
+    [String] $HelpURL
+    [scriptblock] $HelpAction
 
-    KoanAttribute($Position) {
+    KoanAttribute($Position, $HelpURL, $HelpAction) {
         $this.Position = $Position
+        $this.HelpURL = $HelpURL
+        $this.HelpAction = $HelpAction
+    }
+
+    KoanAttribute($Position, $HelpURL) {
+        $this.KoanAttribute($Position, $HelpURL)
+    }
+    
+    KoanAttribute($Position) {
+        $this.KoanAttribute($Position, $null, $null)
     }
 
     KoanAttribute() {
-        $this.Position = [uint32]::MaxValue
+        $this.KoanAttribute([uint32]::MaxValue, $null, $null)
     }
 }
