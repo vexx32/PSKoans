abbc8523f3a63570915b59de6240fb582fb9378a
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-11-10 12:05:35 -0500

    Create 'Should -Fail' operator for deliberate fails

--- PSKoans/PSKoans.psm1
+++ PSKoans/PSKoans.psm1
@@ -45,8 +45,34 @@ Get-ChildItem "$PSScriptRoot/Public", "$PSScriptRoot/Private" | ForEach-Object {
 $env:PSKoans_Folder = $Home | Join-Path -ChildPath 'PSKoans'
 Write-Verbose "Koans folder set to $env:PSKoans_Folder"
 
-
 if (-not (Test-Path -Path $env:PSKoans_Folder)) {
     Write-Verbose 'Koans folder does not exist; populating the folder'
     Initialize-KoanDirectory -Confirm:$false
+}
+
+Add-AssertionOperator -Name Fail -Test {
+    param ($ActualValue, [switch] $Negate, [string] $Because)
+
+    # look at  https://github.com/pester/Pester/blob/master/Functions/Assertions/BeTrueOrFalse.ps1
+    # for inspiration, or here https://mathieubuisson.github.io/pester-custom-assertions/
+
+    if ($Negate) {
+        return [PSCustomObject]@{
+            Succeeded      = $false
+            FailureMessage = "Should -Not -Fail is not a valid assertion."
+        }
+    }
+
+    if (-not $Because) {
+        $Message = "The test failed, because the script reached the Should -Fail command call."
+    }
+    else {
+        $Reason = ($Because.Trim().TrimEnd('.') -replace '^because\s', '')
+        $Message = "The test failed, because $Reason."
+    }
+
+    [PSCustomObject]@{
+        Succeeded      = $false
+        FailureMessage = $Message
+    }
 }
