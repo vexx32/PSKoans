28ba555792d0474b6854ee1cbdee4ad7cad1e8a2
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-11-21 20:30:53 -0500

    Update build process to work with Azure and publish to gallery

--- Deploy.PSDeploy.ps1
+++ Deploy.PSDeploy.ps1
@@ -1,11 +1,9 @@
-if ($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1) {
-    Deploy Module {
-        By PSGalleryModule {
-            FromSource $ENV:BHProjectName
-            To PSGallery
-            WithOptions @{
-                ApiKey = $ENV:NugetApiKey
-            }
+Deploy Module {
+    By PSGalleryModule {
+        FromSource $ENV:BHProjectName
+        To PSGallery
+        WithOptions @{
+            ApiKey = $ENV:NugetApiKey
         }
     }
 }
