0a39138f790837907f40d976da6f086d52034baa
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2018-12-10 23:39:25 -0500

    v0.42.2 - BulletProofing. I think. (#105)
    * Use short inherited type name
    This fixes the issue where the type was not loading correctly
    I don't quite understand how, but it works!
    
    * Use runspaces for Invoke-Koan
    This allow us to apply "using module" properly for the koan files
    
    * Shorten type names for clarity
    
    * Modify koan file finding to be a little more reliable

--- PSKoans/PSKoans.psm1
+++ PSKoans/PSKoans.psm1
@@ -18,10 +18,10 @@
     class FILL_ME_IN {}
     class __ : FILL_ME_IN {}
 
-    class KoanAttribute : System.Attribute {
+    class KoanAttribute : Attribute {
         [uint32] $Position
 
-        KoanAttribute($Position) {
+        KoanAttribute([uint32] $Position) {
             $this.Position = $Position
         }
 
@@ -50,29 +50,33 @@ if (-not (Test-Path -Path $env:PSKoans_Folder)) {
     Initialize-KoanDirectory -Confirm:$false
 }
 
-Add-AssertionOperator -Name Fail -Test {
-    param ($ActualValue, [switch] $Negate, [string] $Because)
 
-    # look at  https://github.com/pester/Pester/blob/master/Functions/Assertions/BeTrueOrFalse.ps1
-    # for inspiration, or here https://mathieubuisson.github.io/pester-custom-assertions/
+try {
+    Add-AssertionOperator -Name Fail -Test {
+        param ($ActualValue, [switch] $Negate, [string] $Because)
 
-    if ($Negate) {
-        return [PSCustomObject]@{
+        # look at  https://github.com/pester/Pester/blob/master/Functions/Assertions/BeTrueOrFalse.ps1
+        # for inspiration, or here https://mathieubuisson.github.io/pester-custom-assertions/
+
+        if ($Negate) {
+            return [PSCustomObject]@{
+                Succeeded      = $false
+                FailureMessage = "Should -Not -Fail is not a valid assertion."
+            }
+        }
+
+        if (-not $Because) {
+            $Message = "The test failed, because the script reached the Should -Fail command call."
+        }
+        else {
+            $Reason = ($Because.Trim().TrimEnd('.') -replace '^because\s', '')
+            $Message = "The test failed, because $Reason."
+        }
+
+        [PSCustomObject]@{
             Succeeded      = $false
-            FailureMessage = "Should -Not -Fail is not a valid assertion."
+            FailureMessage = $Message
         }
     }
-
-    if (-not $Because) {
-        $Message = "The test failed, because the script reached the Should -Fail command call."
-    }
-    else {
-        $Reason = ($Because.Trim().TrimEnd('.') -replace '^because\s', '')
-        $Message = "The test failed, because $Reason."
-    }
-
-    [PSCustomObject]@{
-        Succeeded      = $false
-        FailureMessage = $Message
-    }
 }
+catch {}
