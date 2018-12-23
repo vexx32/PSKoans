a8c2ee9dc5d339b18677f825377a1a2ab787954b
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-12-22 23:12:02 -0500

    :construction: Layout intro and initial basis for koan lesson

---
+++ PSKoans/Koans/Constructs/AboutPSObjects.Koans.ps1
@@ -0,0 +1,49 @@
+﻿using module PSKoans
+[Koan(Position = 304)]
+param()
+
+<#
+    PSObjects
+
+    Everything in PowerShell is in actuality a PSObject. PSObject is a wrapper type that can contain
+    any other type of boxable object, or none. Not all object types can actually be boxed; byref-like
+    structs (e.g., System.Span) are one particular example.
+
+    PSObjects can be used to add additional members to a basic object type; this is how the Add-Member
+    cmdlet works. It is also quite easy to access the normally-hidden PSObject properties in order
+    to accomplish otherwise difficult tasks, such as dynamically listing any given object's members,
+    as well as adding and removing ETS (instance) properties.
+
+    It is somewhat comparable to C#'s ExpandoObject, but is unrelated.
+#>
+
+Describe 'PSObject' {
+    BeforeAll {
+        $Object = 'Stand for nothing; fall for everything.'
+    }
+
+    It 'is accessed via a hidden .PSObject property' {
+        <#
+            The .PSObject member contains many properties, but the .PSObject property itself does not
+            actually tab-complete, though its child properties do.
+        #>
+        $PSObjectProperties = ($Object.PSObject | Get-Member).Name
+
+        @(
+            __
+            'Copy'
+            'Equals'
+            __
+            'GetMetaObject'
+            __
+            __
+            'ToString'
+            __
+            'ImmediateBaseObject'
+            __
+            'Methods'
+            __
+            __
+        ) | Should -Be $PSObjectProperties
+    }
+}
