82adf0c91cfb844927a3fbf8ad04f73e51ef8f3b
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-01-02 21:35:23 -0500

    TEST: Update Invoke-Koan tests

--- Tests/Functions/Private/Invoke-Koan.Tests.ps1
+++ Tests/Functions/Private/Invoke-Koan.Tests.ps1
@@ -1,4 +1,4 @@
-﻿##Requires -Modules SKoans
+﻿#Requires -Modules SKoans
 
 ${global:Test File} = "$PSScriptRoot/ControlTests/Invoke-Koan.Control_Tests.ps1"
 
@@ -7,16 +7,16 @@ InModuleScope 'PSKoans' {
 
         It 'will not error out' {
             {
-                Invoke-Koan -Script ${global:Test File}
+                Invoke-Koan @{ Script = ${global:Test File} }
             } | Should -Not -Throw
         }
 
         It 'will produce output with -Passthru' {
-            Invoke-Koan -Script ${global:Test File} -PassThru | Should -Not -BeNullOrEmpty
+            Invoke-Koan @{ Script = ${global:Test File} -PassThru } | Should -Not -BeNullOrEmpty
         }
 
         It 'will correctly report test results' {
-            $Results = Invoke-Koan -Script ${global:Test File} -PassThru
+            $Results = Invoke-Koan @{ Script = ${global:Test File} -PassThru }
 
             $Results.TotalCount | Should -Be 2
             $Results.PassedCount | Should -Be 0
@@ -24,7 +24,7 @@ InModuleScope 'PSKoans' {
         }
 
         It 'reports only expected exception types' {
-            $Results = Invoke-Koan -Script ${global:Test File} -PassThru
+            $Results = Invoke-Koan @{ Script = ${global:Test File} -PassThru }
 
             $Results.TestResult.ErrorRecord.Exception |
                 ForEach-Object -MemberName GetType |
