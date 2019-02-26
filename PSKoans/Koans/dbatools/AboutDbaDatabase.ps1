c3ba20f73b90cb6ed6c5aebd217e377f6620ad74
Shaneis <shaneoneillis@gmail.com>
2019-02-26 20:03:42 +0000

    Fixed typo in the description block

--- PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
+++ PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
@@ -5,9 +5,10 @@ param()
 <#
     Get-DbaDatabase
 
-    The Get-DbaDatabase ommand gets SQL database information for each database
-    that is present on the target instance(s) of SQL Server. If the name of the
-    database is provided, the command will return only the specific database.
+    The Get-DbaDatabase command gets SQL database information for each database
+    that is present on the target instance(s) of SQL Server by default. If the 
+    name of the database is provided, the command will return only the specific 
+    database.
 #>
 Describe 'Get-DbaDatabase' {
 
