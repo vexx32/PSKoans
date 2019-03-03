f4cad79689e1cd5346b4332e62447729cb44b66c
Shaneis <shaneoneillis@gmail.com>
2019-03-03 06:46:40 +0000

    Added Koan for using Get-DbaDatabase for all databases.

--- PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
+++ PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
@@ -13,14 +13,21 @@ param()
 Describe 'Get-DbaDatabase' {
 
     Mock -CommandName Get-DbaDatabase -MockWith {
+        Import-Clixml -Path .\PSKoans\Koans\dbatools\Mocks\Database_All.xml
+    } -ParameterFilter { $_.SqlInstance -eq 'localhost' }
+
+    Mock -CommandName Get-DbaDatabase -MockWith {
         Import-Clixml -Path ".\PSKoans\Koans\dbatools\Mocks\Database_TestDb.xml" 
-    } -ParameterFilter { $_.Database -eq 'testdb' }
+    } -ParameterFilter { $_.SqlInstance -eq 'localhost' -and $_.Database -eq 'testdb' }
 
     # `Get-DbaDatabase` requires one thing; A SQL Server instance name.
     # You can pass in "localhost" for the default name for a SQL Server instance.
     # The simplest usage of `Get-DbaDatabase` is to run it and passing in the name
     # of the SQL Server instance. This will get information about all the databases
     # on the instance.
+    $AllDatabases = Get-DbaDatabase -SqlInstance ____
+    $AllDatabases.Count | Should -Be 4
+
     # By passing in the SQL Server instance and the name of a specific database, by
     # using the `-Database` parameter, we can get information on that single
     # database instead.
