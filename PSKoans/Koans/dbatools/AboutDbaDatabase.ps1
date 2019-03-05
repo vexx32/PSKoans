48d9546de2ee2d1f5aded1d06e4d3f27aa9978ff
Shaneis <shaneoneillis@gmail.com>
2019-03-05 21:45:18 +0000

    Removed evidence of my merge conflict resolution.

--- PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
+++ PSKoans/Koans/dbatools/AboutDbaDatabase.ps1
@@ -12,11 +12,6 @@ param()
 #>
 Describe 'Get-DbaDatabase' {
 
-<<<<<<< HEAD
-    Mock -CommandName Get-DbaDatabase -MockWith {
-        Import-Clixml -Path ".\PSKoans\Koans\dbatools\Mocks\Database_TestDb.xml" 
-    } -ParameterFilter { $_.Database -eq 'testdb' }
-=======
     #region Mocks
     Mock -CommandName Get-DbaDatabase -MockWith {
         Import-Clixml -Path .\PSKoans\Koans\dbatools\Mocks\Database_All.xml
@@ -34,38 +29,22 @@ Describe 'Get-DbaDatabase' {
         Import-Clixml -Path .\PSKoans\Koans\dbatools\Mocks\Database_TestDb.xml
     } -ParameterFilter { $_.SqlInstance -eq 'localhost' -and $_.ExludeSystem -eq $true }
     #endregion
->>>>>>> all-databases
 
     # `Get-DbaDatabase` requires one thing; A SQL Server instance name.
     # You can pass in "localhost" for the default name for a SQL Server instance.
     # The simplest usage of `Get-DbaDatabase` is to run it and passing in the name
     # of the SQL Server instance. This will get information about all the databases
     # on the instance.
-<<<<<<< HEAD
-=======
     $AllDatabases = Get-DbaDatabase -SqlInstance ____
     $AllDatabases.Count | Should -Be 5
 
->>>>>>> all-databases
     # By passing in the SQL Server instance and the name of a specific database, by
     # using the `-Database` parameter, we can get information on that single
     # database instead.
     $MasterDatabase = Get-DbaDatabase -SqlInstance localhost -Database ____
     $MasterDatabase.Name | Should -Be 'testdb'
 
-<<<<<<< HEAD
-=======
     # You may want to get only the system databases on an instance. While it is
-    # possible to pass in the names using the -Database parameter:
-
-    # Get-DbaDatabase -SqlInstance localhost -Database 'tempdb','master','model','msdb'
-
-    # Or by using the parameter -ExcludeDatabase to exclude 'testdb':
-
-    # Get-DbaDatabase -SqlInstance localhost -ExcludeDatabase testdb
-
-    # Instead of writing out all the database names, it is easier to use the switch
-    # -ExcludeUser that is provided by the command, especially if there are multiple
     # different databases on the instance.
     $UserDbsExcluded = Get-DbaDatabase -SqlInstance localhost -____
     $UserDbsExcluded | Select-Object -ExpandProperty Name | Should -BeIn 'tempdb', 'master', 'model', 'msdb'
@@ -81,5 +60,4 @@ Describe 'Get-DbaDatabase' {
     # recovery models.
     $FullRecoveryDbs = Get-DbaDatabase -SqlInstance localhost -RecoveryModel ____
     $FullRecoveryDbs | Select-Object -ExpandProperty RecoveryModel | Should -Be 'Full'
->>>>>>> all-databases
 }
