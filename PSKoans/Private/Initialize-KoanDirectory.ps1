6df9782d0bd35b9ad997bbe6a7e964b1ac7ff31f
Joel <32407840+vexx32@users.noreply.github.com>
2019-03-29 13:43:48 -0400

    :sparkles: Add -Topic to Initialize-KoanDirectory
    :memo: Add new param to CBH

--- PSKoans/Private/Initialize-KoanDirectory.ps1
+++ PSKoans/Private/Initialize-KoanDirectory.ps1
@@ -5,6 +5,8 @@
 	.DESCRIPTION
         If Koans folder already exists, the folder(s) are overwritten. Otherwise a new folder
         structure is produced.
+    .PARAMETER Topic
+        Specifies specific koan topics to reset to the blank state.
     .NOTES
         Author: Joel Sallow
         Module: PSKoans
@@ -13,21 +15,42 @@
     #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
     [OutputType([void])]
-    param()
+    param(
+        [Parameter()]
+        [string[]]
+        $Topic
+    )
 
     $KoanFolder = Get-PSKoanLocation
 
-    if ($PSCmdlet.ShouldProcess($KoanFolder, "Restore the koans to a blank slate")) {
-        if (Test-Path -Path $KoanFolder) {
-            Write-Verbose "Removing the entire koans folder..."
-            Remove-Item -Recurse -Path $KoanFolder -Force
-        }
+    if ($Topic) {
+        $KoanList = Get-ChildItem -Path $KoanFolder -Recurse -Filter *.Koans.ps1 |
+            Where-Object { $_.BaseName -replace '\.Koans$' -in $Topic }
 
-        Write-Debug "Copying koans to folder"
-        Copy-Item -Path "$script:ModuleRoot/Koans" -Recurse -Destination $KoanFolder
-        Write-Verbose "Koans copied to '$KoanFolder'"
+        foreach ($Koan in $KoanList) {
+            $TopicName = $Koan.Basename -replace '\.Koans$'
+
+            if ($PSCmdlet.ShouldProcess($TopicName, "Reset koan lesson")) {
+                $OriginalFile = Get-ChildItem -Path $script:ModuleRoot -Recurse -Filter $Koan.Name
+
+                Write-Verbose "Restoring $TopicName to a blank slate"
+                $OriginalFile | Copy-Item -Destination $Koan.Fullname -Force
+            }
+        }
     }
     else {
-        Write-Verbose "Operation cancelled; no modifications made to koans folder."
+        if ($PSCmdlet.ShouldProcess($KoanFolder, "Restore the koans to a blank slate")) {
+            if (Test-Path -Path $KoanFolder) {
+                Write-Verbose "Removing the entire koans folder..."
+                Remove-Item -Recurse -Path $KoanFolder -Force
+            }
+
+            Write-Debug "Copying koans to folder"
+            Copy-Item -Path "$script:ModuleRoot/Koans" -Recurse -Destination $KoanFolder
+            Write-Verbose "Koans copied to '$KoanFolder'"
+        }
+        else {
+            Write-Verbose "Operation cancelled; no modifications made to koans folder."
+        }
     }
 }
