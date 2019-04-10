28a79334d34d5c0c8caa02ee3295d01d338c16c7
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-04-09 23:40:55 -0400

    :construction: Add a few koans

--- PSKoans/Koans/Constructs and Patterns/AboutErrorHandling.Koans.ps1
+++ PSKoans/Koans/Constructs and Patterns/AboutErrorHandling.Koans.ps1
@@ -10,17 +10,45 @@ param()
 
     Errors can be flagged as terminating or non-terminating based on a few different criteria, largely
     determined by the command that creates the error. Terminating errors can be paired with try/catch
-    patterns that
+    patterns that allow more granular control over exactly how the error is handled.
 #>
 
 Describe 'ErrorRecord' {
-
-    It 'contains a reference to an Exception' {
-
+    BeforeAll {
+        $Record = try {
+            throw "A challenge to the sky!"
+        }
+        catch {
+            $_
+        }
     }
 
-    It 'often has a reference to the object that caused the error' {
+    It 'is an ErrorRecord' {
+        # At times, even tautologies can make sense of what is, and is not.
 
+        $Record -is [__] | Should -BeTrue
+    }
+
+    It 'always contains a reference to an Exception' {
+        $Record.Exception | Should -Not -BeNullOrEmpty
+        $Record.Exception -is [__] | Should -BeTrue
+        "__" | Should -Be $Record.Exception.Message
+    }
+
+    It 'sometimes has a reference to the object that caused the error' {
+        # But not always!
+        $Record.TargetObject | Should -BeNullOrEmpty
+
+        # Non-terminating error behaviour can be adjusted with the -ErrorAction parameter.
+        Get-Item -Path "TestDrive:\This_Shouldn't_Exist" -ErrorAction SilentlyContinue
+
+        <#
+            $Error is an automatic variable that contains a list of recent errors. $Error[0] is the most recent.
+            Even if an error is silenced with -ErrorAction SilentlyContinue, it is still recorded in $Error.
+
+            If you want to completely hide the error, you'll need to use -ErrorAction Ignore instead.
+        #>
+        '__' | Should -Be $Error[0].TargetObject
     }
 
     It 'can be assigned one of the preset categories' {
