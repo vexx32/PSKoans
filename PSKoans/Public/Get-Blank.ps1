e0f3955e8db825c6965b72a3b68f61967e62f9c0
Joel <32407840+vexx32@users.noreply.github.com>
2019-04-24 09:14:58 -0400

    :sparkles: New alias for Get-Blank

--- PSKoans/Public/Get-Blank.ps1
+++ PSKoans/Public/Get-Blank.ps1
@@ -1,7 +1,7 @@
 ﻿function Get-Blank {
     [CmdletBinding(HelpUri = 'https://github.com/vexx32/PSKoans/tree/master/docs/Get-Blank.md')]
     [OutputType('Blank')]
-    [Alias('__', 'FILL_ME_IN')]
+    [Alias('__', '____', 'FILL_ME_IN')]
     param()
 
     Write-Verbose "I AIN'T DOIN' NOTHIN'!!!"
