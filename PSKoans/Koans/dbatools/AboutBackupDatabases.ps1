10f6827459db41ea808290668ab55e7a6b1690f3
shaneis <shaneoneillis@gmail.com>
2019-07-25 10:32:12 +0100

    Started Backup-DbaDatabase

---
+++ PSKoans/Koans/dbatools/AboutBackupDatabases.ps1
@@ -0,0 +1,9 @@
+#Requires -Modules dbatools
+using module PSKoans
+[Koan(Position = 1003)]
+param()
+<#
+    Backup-DbaDatabase
+
+    Backing up a database is crucially important to all who use databases.
+#>
