d00f5ce4c8525f12066a70eb96096ef496ed9985
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-08-31 23:08:07 -0400

    :wrench: Fix #176 AboutTeeObject Variables are not created till the completion of the pipeline Reframe some assertions appropriately Add some comments clarifying a few things Reformat blanks to current standard

--- PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
@@ -12,21 +12,24 @@ Describe 'Tee-Object' {
 
     It 'passes the object on through the pipeline' {
         # The value passed to -Variable is created as a new variable
-        1..10 | Tee-Object -Variable __ | Should -Be $n
+        $Value = 10
+        __ | Tee-Object -Variable Tee | Should -Be $Value
     }
 
     It 'can store the object(s) into a variable' {
-        # Note the variable name is passed as a string, without the $!
-        'alpha', 'beta', 'gamma' | Tee-Object -Variable 'Numbers' | Should -Be $Numbers
-        '__' | Should -Be $Numbers[1]
+        # Note the variable name is given as a string, without the $ prefix.
+        $Values = 'alpha', 'beta', 'gamma'
+        @('____', '____', '____', '____') | Tee-Object -Variable 'Numbers' | Should -Be $Values
+        '____' | Should -Be $Numbers[1]
     }
 
     It 'can also store the object(s) into a file' {
         $File = New-TemporaryFile
 
-        $Output = 1..5 | ForEach-Object {"{0:N2}" -f (1 / $_)} | Tee-Object -FilePath $File.FullName
-        $Stored = $File | Get-Content
+        $Output = 1..5 | ForEach-Object { "{0:N2}" -f (1 / $_) } | Tee-Object -FilePath $File.FullName
+        $Stored = Get-Content -Path $File
 
-        __ | Should -Be $Stored
+        # Text files can only store string data, so be careful storing arbitrary data to files like this.
+        @('__', '__', '3', '__', '__') | Should -Be $Stored
     }
 }
