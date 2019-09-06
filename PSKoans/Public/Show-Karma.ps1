c0715917f48c60d8d9e252f47055db6dd91a1fe7
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-09-06 00:31:03 -0400

    :sparkles: Add Get-Karma command Move internal logic from Show-Karma to Get-Karma where reasonable

--- PSKoans/Public/Show-Karma.ps1
+++ PSKoans/Public/Show-Karma.ps1
@@ -46,11 +46,7 @@
     )
     switch ($PSCmdlet.ParameterSetName) {
         'ListKoans' {
-            Get-PSKoanLocation |
-                Get-ChildItem -Recurse -File -Filter '*.Koans.ps1' |
-                ForEach-Object {
-                    $_.BaseName -replace '\.Koans$'
-                }
+            Get-PSKoanFile
         }
         'OpenFolder' {
             Write-Verbose "Opening koans folder"
@@ -81,98 +77,10 @@
 
             Show-MeditationPrompt -Greeting
 
-            Write-Verbose 'Sorting koans...'
-            try {
-                if ($Topic) {
-                    Write-Verbose "Getting Koans matching selected topic(s): $($Topic -join ', ')"
-                    $SortedKoanList = Get-Koan -Topic $Topic
-                }
-                else {
-                    $SortedKoanList = Get-Koan
-                }
-            }
-            catch {
-                $PSCmdlet.ThrowTerminatingError($_)
-            }
+            $Meditation = if ($Topic) { Get-Karma -Topic $Topic } else { Get-Karma }
 
-            Write-Verbose "Koan files retrieved: $($SortedKoanList.Count)"
-            Write-Verbose 'Counting koans...'
-            [int]$TotalKoans = $SortedKoanList | Measure-Koan
-
-            if ($TotalKoans -eq 0) {
-                if ($Topic) {
-                    $ErrorDetails = @{
-                        ExceptionType    = 'System.IO.FileNotFoundException'
-                        ExceptionMessage = 'Could not find any koans that match the specified Topic(s)'
-                        ErrorId          = 'PSKoans.NoMatchingKoansFound'
-                        ErrorCategory    = 'ObjectNotFound'
-                        TargetObject     = $Topic -join ','
-                    }
-                    $PSCmdlet.ThrowTerminatingError( (New-PSKoanErrorRecord @ErrorDetails) )
-                }
-
-                # Something's wrong; possibly a koan folder from older versions, or a folder exists but has no files
-                Write-Warning 'No koans found in your koan directory. Initiating full reset...'
-                Update-PSKoan -Confirm:$false
-                Measure-Karma @PSBoundParameters # Re-call ourselves with the same parameters
-
-                return # Skip the rest of the function
-            }
-
-            $KoansPassed = 0
-
-            foreach ($KoanFile in $SortedKoanList) {
-                Write-Verbose "Testing karma with file [$($KoanFile.Path)]"
-
-                # Execute in a fresh scope to prevent internal secrets being leaked
-                $PesterTests = Invoke-Koan @{
-                    Script   = $KoanFile.Path
-                    PassThru = $true
-                    Show     = 'None'
-                }
-
-                $KoansPassed += $PesterTests.PassedCount
-
-                Write-Verbose "Karma: $KoansPassed"
-                if ($PesterTests.FailedCount -gt 0) {
-                    Write-Verbose 'Your karma has been damaged.'
-                    break
-                }
-            }
-
-            $Meditation = if ($PesterTests.FailedCount -gt 0) {
-                $NextKoanFailed = $PesterTests.TestResult |
-                    Where-Object Result -eq 'Failed' |
-                    Select-Object -First 1
-
-                @{
-                    DescribeName = $NextKoanFailed.Describe
-                    Expectation  = $NextKoanFailed.ErrorRecord
-                    ItName       = $NextKoanFailed.Name
-                    Meditation   = $NextKoanFailed.StackTrace
-                    KoansPassed  = $KoansPassed
-                    TotalKoans   = $TotalKoans
-                    CurrentTopic = @{
-                        Name      = $KoanFile.Name -replace '\.Koans\.ps1$'
-                        Completed = $PesterTests.PassedCount
-                        Total     = $PesterTests.TotalCount
-                    }
-                }
-            }
-            else {
-                @{
-                    Complete    = $true
-                    KoansPassed = $KoansPassed
-                    TotalKoans  = $TotalKoans
-                }
-            }
-
-            if ($Detailed) {
-                $Meditation.Add('Results', $PesterTests.TestResult)
-            }
-
-            if ($PSBoundParameters.ContainsKey('Topic')) {
-                $Meditation.Add('RequestedTopic', $Topic)
+            if (-not $Detailed) {
+                $Meditation.Remove('Results')
             }
 
             Show-MeditationPrompt @Meditation
