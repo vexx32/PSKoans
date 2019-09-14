8a62344f40c0f763ca4ee2f65f56ac59eaf5b317
Benjamin Clark <bclark@sudoblark.com>
2019-09-14 21:40:03 +0100

    Update AboutBooleans 01
    - Related to PR #241
    - Minor changes in-line with PR Owner requests

--- PSKoans/Koans/IntroTraining/AboutBooleans.koans.ps1
+++ PSKoans/Koans/IntroTraining/AboutBooleans.koans.ps1
@@ -4,8 +4,7 @@ param()
 <#
     Booleans
 
-    Booleans in PowerShell are either True or False, 1 or 0, on or off.
-
+    Booleans in PowerShell are either True or False, 1 or 0, on or off. 
     They allow us to represent a logical, binary, state of being. 
 
     "Can I stop this service?" The answer is yes or no.
@@ -16,19 +15,13 @@ param()
 
     Check for yourself by running the following code:
 
-    Get-Service | Get-Member
+        Get-Service | Get-Member
 
     Then scroll down till you see the "CanStop" property:
 
     CanStop                   Property      bool CanStop {get;}
 
-    Bools are either:
-    
-    $True
-
-    Or:
-
-    $False
+    There are two possible values for a boolean: $true or $false
 
 #>
 
@@ -38,34 +31,20 @@ Describe "Booleans" {
     Using only booleans, so $True or $False, answer the following questions
 
 #>
-
     It "True or False" {
 
-        # Is one greater than two? 
-        # Replace "__" with $True or $False
-        "__" -eq (1 -gt 2) | should -be $true -Because "1 is greater than two"
+        # Replace the blanks with $true or $false
+        $____ | Should -Be (1 -gt 2) -Because '1 is not greater than 2'
 
-        # Is one less than two
-        # Replace "__" with $True or $False
-        "__" -eq (1 -lt 2) | should -be $true -Because "1 is greater than two"
+        $____ | Should -Be (1 -lt 2) -Because "1 is greater than two"
 
-        # Is ten less than twenty?
-        # Replace "__" with $True or $False
-        "__" -eq (10 -lt 20) | should -be $true -Because "10 is less than 20"
+        $____ | Should -Be (10 -lt 20) -Because "10 is less than 20"
 
-        # Is ten greater than twenty?
-        # Replace "__" with $True or $False
-        "__" -eq (10 -gt 20) | should -be $true -Because "10 is less than 20"
+        $____ | Should -Be (10 -gt 20) -Because "10 is less than 20"
+        
+        $____ | Should -Be (3 -eq 3) -Because "3 is equal to 3"
 
-        # Is three the same as three?
-        # Replace "__" with $True or $False
-        "__" -eq (3 -eq 3) | should -be $true -Because "3 is equal to 3"
-
-        # Is 100 less than 1?
-        # Replace "__" with $True or $False
-        "__" -eq (100 -lt 1) | should -be $true -Because "100 is less than one"
+        $____ | Should -Be (100 -lt 1) -Because "100 is less than one"
 
     }
-
-
 }
