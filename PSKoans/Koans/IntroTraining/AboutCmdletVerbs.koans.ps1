09534c53fde06ec85c331f8ed6a9b3074d0da4fa
Ben Clark <bclark@sudoblark.com>
2019-09-16 13:21:18 +0100

    Update AboutCmdletVerbs 02
    - Relates to PR #241
    - Added reference to functions
    - Added reference to Get-Verb
    - Intended code examples
    - Remove answer module
    - Reframed Koans

--- PSKoans/Koans/IntroTraining/AboutCmdletVerbs.koans.ps1
+++ PSKoans/Koans/IntroTraining/AboutCmdletVerbs.koans.ps1
@@ -4,27 +4,24 @@ param()
 <#
     Cmdlet Verbs
 
-    The fundamental building block of PowerShell is cmdlets. Whilst the following may hold true
-    for 99% of PowerShell, it may not apply to Exchange. This is as the Exchange Console was 
-    developed in parallel to PowerShell. Once all the various standards were ratified, these
-    were not retroactively applied to Exchange Console. So when using that they may be some
-    differences.
+    The fundamental building block of PowerShell are cmdlets and functions.
 
-    Cmdlets are made up of the following syntax:
+    We'll cover functions later, but cmdlets are made up of the following syntax:
 
-    Verb-Noun
+        Verb-Noun
 
     With the verb being an action, and the noun being an object.
 
     In this Koan, we'll cover the 5 basic verbs which'll see you through the majority of your
-    PowerShell needs.
+    PowerShell needs. But PowerShell has more than just 5 predefined verbs! You can see them
+    all by running the following cmdlet:
+
+        Get-Verb
 #>
 
-$RelativePath = ".\AnswerFunctions\IntroTraining\AboutCmdletVerbsAnswers\AboutCmdletVerbsAnswers.psd1"
-Import-Module $RelativePath
+Describe "Basic Verbs" {
 
-Describe 'Verbs' {
-    It 'Get' {
+    Context "Get" {
         <#
             Get
 
@@ -32,21 +29,25 @@ Describe 'Verbs' {
 
             So for example:
 
-            Get-Service
+                Get-Service
 
             Will retrieve all the services running on your current machine.
-
-            Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
-            find 5 new get commands you haven't previously used.
         #>
-            
-        # Replace each __ with the name of a get cmdlet
-        $Answers = "____","____","____","____","____"
-        $Answers | Should -BeIn (Get-Command -Verb Get).Name
+
+        It 'is for commands that retrieve data'  {
+            <#
+                Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
+                find 5 new get commands you haven't previously used.
+            #>
+
+            # Replace each __ with the name of a Get cmdlet
+            $Answers = "____","____","____","____","____"
+            $Answers | Should -BeIn (Get-Command -Verb Get).Name
+        }
     }
 
-    It 'New' {
-        <#
+    Context "New" {
+         <#
             New
 
             The New verb will create an instance of the given noun.
@@ -56,22 +57,28 @@ Describe 'Verbs' {
 
             Before continuing with this excerised, you're advised to run the following code:
 
-            $Path = "YOUR PATH.txt"
-            New-Item -Path $ItemPath -ItemType file
+                $Path = "YOUR PATH.txt"
+                New-Item -Path $ItemPath -ItemType file
 
             This will create a new text file, in the location your specify, and we can use this
             to explain the other verbs later on.
-
-            Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
-            find 5 new new commands you haven't previously used.
         #>
-            
-        # Replace each __ with the name of a new cmdlet
-        $Answers = "____","____","____","____","____"
-        $Answers | Should -BeIn (Get-Command -Verb New).Name
+
+        It 'is for commands that create data' {
+            <#
+                Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
+                find 5 new new commands you haven't previously used.
+            #>
+
+            # Replace each __ with the name of a New cmdlet
+            $Answers = "____","____","____","____","____"
+            $Answers | Should -BeIn (Get-Command -Verb New).Name
+
+        }
+
     }
 
-    It 'Add' {
+    Context "Add" {
         <#
             Add
 
@@ -80,15 +87,11 @@ Describe 'Verbs' {
             If you followed the example in the 'new' test, you can use add to add some
             text to your newly created text file:
             
-            $Path = "YOUR PATH.txt"
-            Add-Content -Path $Path -Value "Sgt. Bash is the best house robot because... fire."
+                $Path = "YOUR PATH.txt"
+                Add-Content -Path $Path -Value "Sgt. Bash is the best house robot because... fire."
             
             Before continuing, run this command several times. See what happens, is it the
             result you expected?
-
-
-
-
 
             You'll see that several lines of text were added to the file. This cmdlet only appends
             information, it does not overwrite. 
@@ -100,18 +103,22 @@ Describe 'Verbs' {
             If you want to grant permissions to somebody who doesn't have any, you use Add. If 
             you want to change permissions for somebody who already has some, you need to use a
             different verb that we'll cover later.
-
-            Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
-            find 5 new add commands you haven't previously used.
         #>
-            
-        # Replace each __ with the name of a add cmdlet
-        $Answers = "____","____","____","____","____"
-        $Answers | Should -BeIn (Get-Command -Verb Add).Name
 
+        It 'is for commands that append data' {
+            <#
+                Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
+                find 5 new add commands you haven't previously used.
+            #>
+
+            # Replace each __ with the name of a Add cmdlet
+            $Answers = "____","____","____","____","____"
+            $Answers | Should -BeIn (Get-Command -Verb Add).Name
+
+        }
     }
 
-    It 'Set' {
+    Context "Set" {
         <#
             Set
 
@@ -120,15 +127,11 @@ Describe 'Verbs' {
             If you followed the example in the 'new' and 'add' tests, you can use set to do
             something to your text file:
             
-            $Path = "YOUR PATH.txt"
-            Set-Content -Path $ItemPath -Value "Sir Kill-A-Lot is the best house robot because of reasons."
+                $Path = "YOUR PATH.txt"
+                Set-Content -Path $ItemPath -Value "Sir Kill-A-Lot is the best house robot because of reasons."
             
             Before continuing, run this command several times. See what happens, is it the
             result you expected?
-
-
-
-
 
             You'll see that there's only one line of text in the file. This is as the Set command
             will overwrite information that's already there.
@@ -145,14 +148,23 @@ Describe 'Verbs' {
             Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
             find 5 new set commands you haven't previously used.
         #>
-            
-        # Replace each __ with the name of a set cmdlet
-        $Answers = "____","____","____","____","____"
-        $Answers | Should -BeIn (Get-Command -Verb Set).Name
+
+        It 'is for commands that overwrite data' {
+            <#
+                Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
+                find 5 new set commands you haven't previously used.
+            #>
+
+            # Replace each __ with the name of a Set cmdlet
+            $Answers = "____","____","____","____","____"
+            $Answers | Should -BeIn (Get-Command -Verb Set).Name
+
+        }
+
     }
 
-    It 'remove' {
-        <#
+    Context "Remove" {
+         <#
             Remove
 
             The Remove verb will delete an instance of an object.
@@ -160,15 +172,11 @@ Describe 'Verbs' {
             If you followed the example in the 'new','add' and 'set' tests, you can use remove to 
             delete your text file:
             
-            $Path = "YOUR PATH.txt"
-            Remove-Item -Path $Path
+                $Path = "YOUR PATH.txt"
+                Remove-Item -Path $Path
             
             Before continuing, run this command several times. What happens when you try to run it
             once the instance has been deleted?
-
-
-
-
 
             You'll see that it fails. Cmdlets with the verb 'remove' simply remove instances of
             an object, if the instance you're referring to doesn't exist then there's nothing
@@ -176,15 +184,18 @@ Describe 'Verbs' {
 
             Returning to Calendar permissions in Office 365, you can use a Remove cmdlet
             to completely remove a user's permissions to a calendar.
-
-            Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
-            find 5 new remove commands you haven't previously used.
         #>
-            
-        # Replace each __ with the name of a remove cmdlet
-        $Answers = "____","____","____","____","____"
-        $Answers | Should -BeIn (Get-Command -Verb Remove).Name
+
+        It "is for commands that delete data" {
+            <#
+                Using the Get-Command cmdlet, which retrieves all the cmdlets currently available,
+                find 5 new remove commands you haven't previously used.
+            #>
+
+            # Replace each __ with the name of a Remove cmdlet
+            $Answers = "____","____","____","____","____"
+            $Answers | Should -BeIn (Get-Command -Verb Remove).Name
+
+        }
     }
 }
-
-Remove-Module $RelativePath
