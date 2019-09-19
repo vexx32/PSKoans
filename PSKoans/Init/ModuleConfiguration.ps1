03f5796da81cefffc6167745511b74b339bb812e
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-09-19 18:31:46 -0400

    :sparkles: Update PSM1 & move init

--- PSKoans/Init/ModuleConfiguration.ps1
+++ PSKoans/Init/ModuleConfiguration.ps1
@@ -1,14 +1,22 @@
 Write-Verbose 'Configuring PSKoans module'
 Write-Verbose 'Importing data strings'
+
 $ShowMeditationPromptData = Import-PowerShellDataFile -Path "$script:ModuleRoot/Data/Show-MeditationPrompt.Data.psd1"
 $script:MeditationStrings = $ShowMeditationPromptData['Koans']
 $script:MeditationPrompts = $ShowMeditationPromptData['Prompts']
 Remove-Variable -Name 'ShowMeditationPromptData'
 
-$script:LibraryFolder = '~/PSKoans'
-Write-Verbose "Koans folder set to $script:LibraryFolder"
+$Configuration = Get-PSKoanSetting
+$LibraryFolder = if ($Configuration.LibraryFolder) {
+    $Configuration.LibraryFolder
+}
+else {
+    Set-PSKoanLocation -Path "~/PSKoans" -PassThru
+}
 
-if (-not (Test-Path -Path $script:LibraryFolder)) {
-    Write-Verbose 'Koans folder does not exist; populating the folder'
+Write-Information "Koans folder set to $LibraryFolder"
+
+if (-not (Test-Path -Path $LibraryFolder)) {
+    Write-Information "Koans folder '$LibraryFolder' was not found; creating koans directory."
     Update-PSKoan -Confirm:$false
 }
