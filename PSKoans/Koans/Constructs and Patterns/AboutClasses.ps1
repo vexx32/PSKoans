e643471bcfc6228bb222df9e3923e264b049ca62
Chris Dent <chris@indented.co.uk>
2019-09-26 15:14:36 +0100

    Creating framework

---
+++ PSKoans/Koans/Constructs and Patterns/AboutClasses.ps1
@@ -0,0 +1,49 @@
+using module PSKoans
+[Koan(Position = 311)]
+param()
+<#
+    About Classes
+
+    A class in .NET might be considered to be the recipe used to create a type. A type
+    is a representation of something, anything. Types are used to describe everything,
+    from strings and numbers to Files, Folders, and Processes.
+
+    PowerShell gained the ability to create classes in PowerShell 5.0, this support
+    continues into PowerShell Core.
+
+    Like the "About Enumerations" topic, jobs are used to isolate example code. Preventing it
+    from affecting the current PowerShell session.
+#>
+Describe 'About Classes' {
+    Context 'Simple Classes' {
+
+    }
+
+    Context 'Properties and Methods' {
+
+    }
+
+    Context 'Static Properties and Methods' {
+
+    }
+
+    Context 'About Hidden' {
+
+    }
+
+    Context 'Constructors' {
+
+    }
+
+    Context 'Inheritance' {
+
+    }
+
+    Context 'Comparison and Equality' {
+
+    }
+
+    Context 'Casting' {
+
+    }
+}
