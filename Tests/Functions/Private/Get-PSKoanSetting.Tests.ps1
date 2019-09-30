1b2d93c8269125ea2abae15abe4fe4d92fcec0f7
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-09-29 22:38:37 -0400

    :white_check_mark: add tests for Get-PSKoanSetting

---
+++ Tests/Functions/Private/Get-PSKoanSetting.Tests.ps1
@@ -0,0 +1,44 @@
+#Requires -Modules PSKoans
+
+Describe 'Get-PSKoanSetting' {
+    BeforeAll {
+        InModuleScope {
+            $script:ConfigPath = 'TestDrive:/config.json'
+        }
+        ${/} = [IO.Path]::DirectorySeparatorChar
+    }
+
+    Context 'Settings file does not exist' {
+        BeforeAll {
+            Mock Set-PSKoanSetting { } -ParameterFilter { $Settings -is [hashtable] } -ModuleName PSKoans
+            $DefaultSettings = InModuleScope PSKoans { $script:DefaultSettings }
+        }
+
+        It 'returns the default settings' {
+            $Result = InModuleScope PSKoans { Get-PSKoanSetting }
+            $Result | Should -BeOfType [hashtable]
+            $Result.KoanLocation | Should -BeExactly "$HOME${/}PSKoans"
+            $Result.Editor | Should -BeExactly 'code'
+        }
+
+        It 'calls Set-PSKoanSetting to set the default settings' {
+            Assert-MockCalled Set-PSKoanSetting -ModuleName PSKoans
+        }
+    }
+
+    Context 'Settings file does exist' {
+        BeforeAll {
+            '{"KoanLocation": "TestLocation","Editor": "TestEditor"}' | Set-Content -Path 'TestDrive:/config.json'
+        }
+
+        It 'returns all settings if none are specified' {
+            $Result = InModuleScope PSKoans { Get-PSKoanSetting }
+            $Result.KoanLocation | Should -BeExactly 'TestLocation'
+            $Result.Editor | Should -BeExactly 'TestEditor'
+        }
+
+        It 'returns only the specified setting with -Name' {
+            InModuleScope PSKoans { Get-PSKoanSetting -Name KoanLocation } | Should -BeExactly 'TestLocation'
+        }
+    }
+}
