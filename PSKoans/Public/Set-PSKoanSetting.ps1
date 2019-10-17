1cdb0a148cff133ab0326f10cd63a3e0cb6bf021
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-10-16 21:37:44 -0400

    :recycle: refactor Set-PSKoanSetting

--- PSKoans/Public/Set-PSKoanSetting.ps1
+++ PSKoans/Public/Set-PSKoanSetting.ps1
@@ -13,71 +13,50 @@ function Set-PSKoanSetting {
 
         [Parameter(Position = 0, Mandatory, ValueFromPipeline, ParameterSetName = 'Multiple')]
         [hashtable]
-        $Settings
-    )
-    if (Test-Path $script:ConfigPath) {
-        switch ($PSCmdlet.ParameterSetName) {
-            'Single' {
-                if ($Name -eq 'Location') {
-                    $Value = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Value)
-                }
+        $Settings,
 
-                (Get-Content -Path $script:ConfigPath) |
-                    ConvertFrom-Json |
-                    Select-Object -Property *, @{ Name = $Name; Expression = { $Value } } -ExcludeProperty $Name |
-                    ConvertTo-Json |
-                    Set-Content -Path $script:ConfigPath
-            }
-            'Multiple' {
-                $Properties = @(
-                    '*'
-                    foreach ($key in $Settings.Keys) {
-                        @{
-                            Name       = $key
-                            Expression = {
-                                if ($Key -eq 'Location') {
-                                    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Settings[$key])
-                                }
-                                else {
-                                    $Settings[$key]
-                                }
-                            }.GetNewClosure()
-                        }
-                    }
-                )
-                (Get-Content -Path $script:ConfigPath) |
-                    ConvertFrom-Json |
-                    Select-Object -Property * -ExcludeProperty $Settings.Keys.ForEach{ $_ } |
-                    Select-Object -Property $Properties |
-                    ConvertTo-Json |
-                    Set-Content -Path $script:ConfigPath
-            }
-        }
+        [Parameter(ParameterSetName = 'Reset')]
+        [switch]
+        $Reset
+    )
+
+    $CurrentSettings = if (Test-Path $script:ConfigPath) {
+        Get-Content -Path $script:ConfigPath | ConvertFrom-Json
     }
     else {
         $ConfigRoot = $script:ConfigPath | Split-Path -Parent
+
         if (-not (Test-Path $ConfigRoot)) {
             New-Item -ItemType Directory -Path $ConfigRoot > $null
         }
 
-        $SettingsToExport = $script:DefaultSettings.Clone()
+        [PSCustomObject]$script:DefaultSettings
+    }
 
-        switch ($PSCmdlet.ParameterSetName) {
-            'Single' {
-                $SettingsToExport[$Name] = $Value
-
-                [PSCustomObject]$SettingsToExport |
-                    ConvertTo-Json |
-                    Set-Content -Path $script:ConfigPath
-            }
-            'Multiple' {
-                $Settings.Keys.ForEach{
-                    $SettingsToExport[$_] = $Settings[$_]
+    $NewSettings = switch ($PSCmdlet.ParameterSetName) {
+        'Single' {
+            $CurrentSettings |
+                Select-Object -Property *, @{ Name = $Name; Expression = { $Value } } -ExcludeProperty $Name
+        }
+        'Multiple' {
+            $Properties = @(
+                '*'
+                foreach ($key in $Settings.Keys) {
+                    @{
+                        Name       = $key
+                        Expression = { $Settings[$key] }.GetNewClosure()
+                    }
                 }
-                [PSCustomObject]$SettingsToExport |
-                    ConvertTo-Json |
-                    Set-Content -Path $script:ConfigPath
-            }
+            )
+            $CurrentSettings |
+                Select-Object -Property $Properties -ExcludeProperty $Settings.Keys
+        }
+        'Reset' {
+            $CurrentSettings
         }
     }
+
+    $NewSettings |
+        ConvertTo-Json |
+        Set-Content -Path $script:ConfigPath
 }
