0c9949db9af744c7660edd013218d79c0d8779f8
Chris Dent <chris@indented.co.uk>
2019-10-23 19:44:39 +0100

    Defining format

---
+++ PSKoans/Koans/Modules/ActiveDirectory/AboutFiltering.Koans.ps1
@@ -0,0 +1,22 @@
+using module PSKoans
+[Koan(Position = 101, Module = 'ActiveDirectory')]
+param()
+<#
+    About Filtering
+
+    The Filter parameter used by the AD module is perhaps one of the most frequently
+    asked about topics.
+
+    The filter parameters uses PowerShell-like syntax which is ultimately used to generate an
+    LDAP filter for a query against Active Directory.
+#>
+Describe 'About Filtering' {
+
+    Context 'PowerShell-like filtering' {
+
+    }
+
+    Context 'LDAP filtering' {
+
+    }
+}
