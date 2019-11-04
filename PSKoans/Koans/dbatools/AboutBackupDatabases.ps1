a83ca237cedcecc727df96fc78003a627752a14e
Shane O'Neill <shane.oneill@kneat.org>
2019-11-04 13:31:10 +0000

    Added opening comments to AboutBackupDatabases.ps1

--- PSKoans/Koans/dbatools/AboutBackupDatabases.ps1
+++ PSKoans/Koans/dbatools/AboutBackupDatabases.ps1
@@ -6,4 +6,12 @@ param()
     Backup-DbaDatabase
 
     Backing up a database is crucially important to all who use databases.
+
+    If there is a need to recover data from an earlier time, then backups get used to recover the data.
+
+    There are many filters and options for backing up a database. dbatools helps you to take a backup
+    with the Backup-DbaDatabase command.
+
+    At it's lowest level, Backup-DbaDatabase can backup every database on the SQL Instance to the default
+    backup directory.
 #>
