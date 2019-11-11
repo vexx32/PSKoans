40bea2562229b16553fade2ba5808fc6c50b66b6
Shane O'Neill <shane.oneill@kneat.org>
2019-11-11 11:03:05 +0000

    Added default mock for Backup-DbaDatabase

--- PSKoans/Koans/dbatools/AboutBackupDatabases.ps1
+++ PSKoans/Koans/dbatools/AboutBackupDatabases.ps1
@@ -15,3 +15,28 @@ param()
     At it's lowest level, Backup-DbaDatabase can backup every database on the SQL Instance to the default
     backup directory.
 #>
+Describe "Backup-DbaDatabase" {
+    <#
+        Let's setup the environment for you. Unless you want the Koans to nearly always fail, I would
+        suggest not messing with this bit.
+    #>
+    Mock -CommandName Backup-DbaDatabase -MockWith {
+        $StartDate = [Datetime]::new(2019, 01, 01, 12, 00, 00)
+        1..10 | ForEach-Object -Process {
+            $RestoreStart = $StartDate
+            $RestoreEnd = $StartDate.AddSeconds(10)
+
+            [PSCustomObject]@{
+                SqlInstance = $ENV:COMPUTERNAME
+                Database = ('Database_{0:d2}' -f $_)
+                Type = 'Full'
+                TotalSize = ({0} -f ([Math]::PI * $_))
+                DeviceType = 'Disk'
+                Start = $RestoreStart
+                Duration = $RestoreEnd - $RestoreStart
+                End = $RestoreEnd
+            }
+            $StartDate = $RestoreEnd
+        }
+    }
+}
