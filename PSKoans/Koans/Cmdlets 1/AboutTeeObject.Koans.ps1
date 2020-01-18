e0ce74be2620492b285dc4068b69c26ee4ceafa5
vexx32 <32407840+vexx32@users.noreply.github.com>
2020-01-17 21:15:04 -0500

    :wrench: Fix issues in AboutTeeObject
    Fixes #341
    
    Also added a new koan to show when the variable is created

--- PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutTeeObject.Koans.ps1
@@ -19,17 +19,42 @@ Describe 'Tee-Object' {
     It 'can store the object(s) into a variable' {
         # Note the variable name is given as a string, without the $ prefix.
         $Values = 'alpha', 'beta', 'gamma'
-        @('____', '____', '____', '____') | Tee-Object -Variable 'Numbers' | Should -Be $Values
+
+        @('____', 'beta', '____') |
+            Tee-Object -Variable 'Numbers' |
+            Should -Be $Values
+
+        '____' | Should -Be $Numbers[0]
         '____' | Should -Be $Numbers[1]
+        'gamma' | Should -Be $Numbers[2]
+    }
+
+    It 'does not create the variable until the pipeline is completed' {
+        $Values = 1..10
+
+        $Values |
+            Tee-Object -Variable Test |
+            ForEach-Object { $Test | Should -BeNullOrEmpty }
+
+        $Test | Should -Not -BeNullOrEmpty
+        $____ | Should -Be $Values
     }
 
     It 'can also store the object(s) into a file' {
-        $File = New-TemporaryFile
+        <#
+            $TestDrive, or TestDrive:, refers to a temporary location that is automatically
+            cleaned out by Pester when the tests are concluded.
+        #>
+        $File = New-Item -Path "$TestDrive/TeeObjectTest.txt"
 
-        $Output = 1..5 | ForEach-Object { "{0:N2}" -f (1 / $_) } | Tee-Object -FilePath $File.FullName
+        1..5 |
+            ForEach-Object { "{0:N2}" -f (1 / $_) } |
+            Tee-Object -FilePath $File.FullName |
+            Out-Null
+
         $Stored = Get-Content -Path $File
 
         # Text files can only store string data, so be careful storing arbitrary data to files like this.
-        @('__', '__', '3', '__', '__') | Should -Be $Stored
+        @('__', '__', '0.33', '__', '__') | Should -Be $Stored
     }
 }
