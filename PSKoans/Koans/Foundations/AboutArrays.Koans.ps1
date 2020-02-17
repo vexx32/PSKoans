22de7b545401dc2a83eb9faf7e59a5bfc9297c21
ChaseFreeman17 <chase4131@gmail.com>
2020-02-16 21:19:33 -0600

    :bug: (#358) Instantiate Stings and Numbers in AboutArrays (#360)

--- PSKoans/Koans/Foundations/AboutArrays.Koans.ps1
+++ PSKoans/Koans/Foundations/AboutArrays.Koans.ps1
@@ -257,16 +257,14 @@ Describe 'Arrays' {
 
             For example, each array has a Contains method.
         #>
-
+        $Numbers = 1, 2, 3, 4
         $____ -eq $Numbers.Contains(3) | Should -BeTrue
 
         # The Contains method is case sensitive for arrays containing strings.
-
+        $Strings = 'first', 'second', 'third'
         $____ -eq $Strings.Contains('first') | Should -BeTrue
         $____ -eq $Strings.Contains('First') | Should -BeTrue
-
         # PowerShell's -contains operator is not case sensitive.
-
         $Strings -contains '____' | Should -BeTrue
         $Strings -contains '____' | Should -BeTrue
     }
