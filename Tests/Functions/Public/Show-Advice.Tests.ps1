528486cfd3d2ebfb5ef499acbbec5c7fac8d7b7a
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-02-26 19:09:28 -0500

    :white_check_mark: Refactor Show-Advice tests
    Use mocks instead of directly modifying the filesystem.
    
    Also use test cases instead of manual for loops.

--- Tests/Functions/Public/Show-Advice.Tests.ps1
+++ Tests/Functions/Public/Show-Advice.Tests.ps1
@@ -3,13 +3,13 @@
 InModuleScope 'PSKoans' {
     Describe "Show-Advice" {
 
-        BeforeAll{
-            Mock Write-ConsoleLine { } 
+        BeforeAll {
+            Mock Write-ConsoleLine { }
             # Exporting incorrect advices to the folder
             $AdviceFolder = $script:ModuleRoot | Join-Path -ChildPath 'Data/Advice'
             $AdviceObject = Get-ChildItem -Path $AdviceFolder -Recurse -File -Filter "*.Advice.json"
             $RandomAdvicesFilePaths = ($AdviceObject | Get-Random -Count 3).FullName
-            $Backup = $RandomAdvicesFilePaths | ForEach-Object {Get-Content $_ | ConvertFrom-Json}
+            $Backup = $RandomAdvicesFilePaths | ForEach-Object { Get-Content $_ | ConvertFrom-Json }
         }
 
         Context "Behaviour of Parameter-less Calls" {
@@ -22,7 +22,7 @@ InModuleScope 'PSKoans' {
             It "calls Write-ConsoleLine with only the display string" {
                 Assert-MockCalled -CommandName Write-ConsoleLine -ParameterFilter { $null -ne $Title } -Times 1
             }
-            
+
             It "outputs nothing to the pipeline" {
                 $result | Should -BeNullOrEmpty
             }
@@ -31,12 +31,12 @@ InModuleScope 'PSKoans' {
         Context "Behaviour with -Name Parameter" {
 
             It "should call Write-ConsoleLine with normal parameters" {
-                Show-Advice -Name "Profile" 
+                Show-Advice -Name "Profile"
                 Assert-MockCalled -CommandName Write-ConsoleLine -ParameterFilter { $null -ne $Title }
             }
 
             It "should call Write-ConsoleLine without parameters" {
-                Show-Advice -Name "Profile" 
+                Show-Advice -Name "Profile"
                 Assert-MockCalled -CommandName Write-ConsoleLine -ParameterFilter { $null -eq $Title }
             }
 
@@ -44,38 +44,44 @@ InModuleScope 'PSKoans' {
                 $message = "Could not find any Advice files matching the specified Name: ThisDoesntExist"
                 { Show-Advice -Name "ThisDoesntExist" -ErrorAction Stop } | Should -Throw -ExpectedMessage $Message
             }
-
-            # Creating incorrect Advice
-            $IncorrectObjectsData = @(
-                @{
-                    NotTitle = "Fake title"
-                    NotContent = @(1..4 | ForEach-Object {"Fake line $_"})
-                },
-                @{
-                    Content = @(1..4 | ForEach-Object {"Fake line $_"})
-                },
-                @{
-                    Title = "Fake title"
-                }
-            )
-            for ($i = 0; $i -lt 3; $i++) {
-                $IncorrectObjectsData[$i] | ConvertTo-Json | Out-File $RandomAdvicesFilePaths[$i]
-            }
-            
-            It "should throw an error if the requested file's format is not correct" {
-                for ($i = 0; $i -lt 3; $i++) {
-                    $AdviceName = (Get-Item $RandomAdvicesFilePaths[$i]).BaseName -replace('^(.*)(?:.Advice)','$1')
-                    $Message = "Could not find Title and/or Content elements for Advice file: {0}" -f $AdviceName
-                    { Show-Advice -Name $AdviceName -ErrorAction Stop } | Should -Throw -ExpectedMessage $Message
-                }
-            }
-
         }
 
-        AfterAll{
-            for ($i = 0; $i -lt 3; $i++) {
-                Set-Content -Path ((Get-Item $RandomAdvicesFilePaths[$i]).FullName) -Value ($Backup[$i] | ConvertTo-Json)
+        Context 'Behaviour with malformed advice files' {
+
+            BeforeAll {
+                $MalformedAdviceCases = @(
+                    @{
+                        Json = @{
+                            NotTitle   = "Fake title"
+                            NotContent = @(1..4 | ForEach-Object { "Fake line $_" })
+                        } | ConvertTo-Json
+                    }
+                    @{
+                        Json = @{
+                            Content = @(1..4 | ForEach-Object { "Fake line $_" })
+                        } | ConvertTo-Json
+                    }
+                    @{
+                        Json = @{
+                            Title = "Fake title"
+                        } | ConvertTo-Json
+                    }
+                )
+
+                $GetContentResult = [string]::Empty
+
+                Mock Get-Content -MockWith { $GetContentResult }
+                Mock Get-ChildItem -MockWith { [PSCustomObject]@{ PSPath = "DummyPath" } }
+            }
+
+            It "should throw an error if the requested file's format is not correct" -TestCases $MalformedAdviceCases {
+                param($Json)
+
+                $GetContentResult = $Json
+                $AdviceName = "TestAdvice"
+                $Message = "Could not find Title and/or Content elements for Advice file: {0}" -f $AdviceName
+                { Show-Advice -Name $AdviceName -ErrorAction Stop } | Should -Throw -ExpectedMessage $Message
             }
         }
-    }   
+    }
 }
