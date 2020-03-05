4baabcde5b58fc8c629019909e55bb35bb4fb5fb
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2020-03-04 23:19:38 -0600

    Refactor Introduction Koans (#370)
    * :recycle: :memo: update AboutAssertions
    
    * :recycle: :art: Refactor AboutBinary
    
    * :memo: Refactor AboutBooleans intro text
    
    * :memo: first pass of AboutArrays
    
    * :recycle: :art: Refactor AboutStrings
    
    * :recycle: :art: :sparkles: AboutNumbers
    - Some refactoring and rewriting of comments.
    - Added mention of suffixes and Long type.

--- PSKoans/Koans/Introduction/AboutAssertions.Koans.ps1
+++ PSKoans/Koans/Introduction/AboutAssertions.Koans.ps1
@@ -5,20 +5,21 @@ param()
     Getting Started
 
     The PowerShell Koans are a set of exercises designed to get you familiar
-    with PowerShell. By the time you're done, you'll have a basic
-    understanding of the syntax of PoSH and learn a little more
-    about scripting in general.
+    with PowerShell. By the time you're done, you'll have a basic understanding
+    of the syntax of PowerShell and learn a little more about scripting. This is
+    where the fun begins! Each koan contains an example designed to teach you
+    something about PowerShell.
 
-    Answering Problems
+    Solving Problems
 
-    This is where the fun begins! Each koan contains an example designed
-    to teach you a lesson about PowerShell. If you execute the program
-    defined in this project, you will get a message that the koan below
-    has failed. Your job is to fill in the blanks (the __ or ____ symbols)
-    to make it pass. Once you make the change, call Show-Karma to make sure
-    the koan passes, and continue on to the next failing koan.
-    With each passing koan, you'll learn more about PowerShell, and add
-    another tool to your PowerShell scripting belt.
+    If you execute the `Show-Karma` command in your PowerShell console, you will
+    get a message that one of the assertions below has failed. Your job is to
+    fill in the blanks (the __ / ____ / $____ / '____' tokens) to make it pass.
+
+    Once you make the change(s) necessary and save the file, call `Show-Karma`
+    to make sure the koan passes, and continue on to the next failing koan. With
+    each passing koan, you'll learn more about PowerShell, and add another tool
+    to your PowerShell scripting belt.
 #>
 Describe 'Equality' {
 
--- PSKoans/Koans/Introduction/AboutBooleans.Koans.ps1
+++ PSKoans/Koans/Introduction/AboutBooleans.Koans.ps1
@@ -4,29 +4,31 @@ param()
 <#
     Booleans
 
-    Booleans in PowerShell are either True or False, 1 or 0, on or off.
-    They allow us to represent a logical, binary, state of being.
+    Booleans in PowerShell are either $true or $false, 1 or 0, on or off. They
+    allow us to represent a logical, binary, state. Effectively, they represent
+    the state of a single bit of data. For example:
 
-    "Can I stop this service?" The answer is yes or no.
+        | "Can I stop this service?"
 
-    Therefore, the CanStop property for a System.ServiceProcess.ServiceController object
-    is a Boolean, abbreviated to bool. In other words, the only possible responses we have
-    to the question, "Can I stop this service?" are either "yes" or "no".
+    The answer is yes or no, True or False.
+
+    One example of a boolean property would be the Responding property on a
+    System.Diagnostics.Process object (which is the output from Get-Process).
+    This property can only show whether the process is responding (True) or not
+    (False).
 
     Check for yourself by running the following code:
 
-        Get-Service | Get-Member
+        Get-Process | Get-Member
 
-    Then scroll down till you see the "CanStop" property:
+    Then scroll down till you see the "Responding" property:
 
-    CanStop                   Property      bool CanStop {get;}
-
-    There are only two possible values for a boolean: $true and $false
+    Responding                 Property       bool Responding {get;}
 #>
 
 Describe "Booleans" {
 
-    # Using only booleans, so either $true or $false, fill in the blanks below.
+    # Using only booleans, either $true or $false, fill in the blanks below.
 
     It '( 1 -gt 2 ) is either true or false' {
         $____ | Should -Be ( 1 -gt 2 ) -Because '1 is not greater than 2'
