2d64c140de0b0cf21bac99393ab71cdd2a9758b7
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-05 22:24:22 -0500

    :white_check_mark: Add regression test
    We can test file ASTs during our static analysis checks.
    
    Added a test to check we don't have any nested It blocks in the file.

--- Tests/KoanValidation.Tests.ps1
+++ Tests/KoanValidation.Tests.ps1
@@ -1,5 +1,8 @@
 #Requires -Modules PSKoans
 
+using namespace System.Management.Automation.Language
+using namespace System.Collections.Generic
+
 $ProjectRoot = Resolve-Path "$PSScriptRoot/.."
 $KoanFolder = $ProjectRoot |
     Join-Path -ChildPath 'PSKoans' -AdditionalChildPath 'Koans'
@@ -30,8 +33,57 @@ Describe 'Koan Topics Static Analysis Checks' {
             $File.FullName | Should -Exist
 
             $Errors = $Tokens = $null
-            [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$Tokens, [ref]$Errors) > $null
+            [Parser]::ParseFile($file.FullName, [ref]$Tokens, [ref]$Errors) > $null
             $Errors.Count | Should -Be 0
+        }
+
+        It 'Koan Topic <Name> should not have nested It blocks' -TestCases $KoanTopics {
+            param($File)
+
+            function Test-ItBlock {
+                [CmdletBinding()]
+                param([Ast] $element)
+
+                if ($element -isnot [CommandAst]) {
+                    return $false
+                }
+
+                $commandName = $element.GetCommandName()
+                if ([string]::IsNullOrEmpty($commandName)) {
+                    return $false
+                }
+
+                if ($commandName -ne 'it') {
+                    return $false
+                }
+
+                return $true
+            }
+
+            $Errors = $Tokens = $null
+            $Ast = [Parser]::ParseFile($file.FullName, [ref]$Tokens, [ref]$Errors)
+            $ParentItBlocks = [HashSet[Ast]]::new()
+
+            $null = $Ast.FindAll(
+                {
+                    param([Ast] $currentAst)
+
+                    if (-not (Test-ItBlock $currentAst)) {
+                        return $false
+                    }
+
+                    for ($node = $currentAst.Parent; $null -ne $node; $node = $node.Parent) {
+                        if (Test-ItBlock $node) {
+                            return $ParentItBlocks.Add($node)
+                        }
+                    }
+
+                    return $false
+                },
+                <# searchNestedScriptBlocks: #> $true
+            )
+
+            $ParentItBlocks | Should -BeNullOrEmpty -Because 'It blocks cannot be nested'
         }
 
         It 'Koan Topic <Name> should include one (and only one) line feed at end of file' -TestCases $KoanTopics {
