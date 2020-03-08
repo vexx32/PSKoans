c2b2d40e4b240c98416bf49902c2ba21b8ae4c85
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-03-08 01:56:30 -0500

    :sparkles: add verbose/debug section

--- PSKoans/Koans/Constructs and Patterns/AboutAdvancedFunctions.Koans.ps1
+++ PSKoans/Koans/Constructs and Patterns/AboutAdvancedFunctions.Koans.ps1
@@ -23,24 +23,12 @@ Describe 'CmdletBinding' {
         functions. It has the effect of turning them into "advanced" functions.
 
         What this actually means is a bit complex, but in essence it brings them
-        significantly closer to behaving like a true compiled cmdlet:
+        significantly closer to behaving like a true compiled cmdlet.
 
-        - Parameters must be explicitly defined. There is no usage of $args, nor
-            any ability to handle unbound parameters. Parameters that can't be
-            bound by position or name will result in an error.
-        - Several automatic parameters are given to the function:
-            -Verbose
-            -Confirm
-            -Debug
-            -WarningAction
-            -WarningVariable
-            -ErrorAction
-            -ErrorVariable
-            -InformationAction
-            -InformationVariable
-            -OutVariable
-            -OutBuffer
-            -PipelineVariable
+        When using CmdletBinding, parameters must be explicitly defined. There
+        is no usage of $args, nor any ability to handle unbound parameters.
+        Parameters that can't be bound by position or name will result in an
+        error.
     #>
 
     Context 'Binding Parameters' {
@@ -79,55 +67,101 @@ Describe 'CmdletBinding' {
             { Test-Function -Some Things -Are Strict } | Should -Throw -ExpectedMessage $Message
         }
     }
+}
 
-    Context 'Common Parameters' {
+Describe 'Common Parameters' {
+    <#
+        When using CmdletBinding to create an advanced function, several
+        automatic parameters are automatically given to the function:
 
-        It 'Provides the -Verbose common parameter' {
+            -Verbose
+            -Debug
+            -WarningAction
+            -WarningVariable
+            -ErrorAction
+            -ErrorVariable
+            -InformationAction
+            -InformationVariable
+            -OutVariable
+            -OutBuffer
+            -PipelineVariable
+    #>
 
+    It 'provides the -Verbose and -Debug common parameters' {
+        <#
+            -Verbose is a parameter that requests additional detail on a
+            function's actions during processing. This information may be
+            useful for debugging at times, but is also used as a basic kind
+            of progress and additional metadata output.
+
+            -Debug is very similar, but intended specifically for debug
+            messages. In Windows PowerShell, -Debug mode will actually
+            prompt before coninuing for every debug message emitted. This
+            behaviour has changed in recent versions of PowerShell (6.2+)
+            and it behaves more like a detailed and focused version of
+            -Verbose.
+        #>
+
+        function Test-Verbose {
+            [CmdletBinding()]
+            param()
+
+            <#
+                A call to Write-Verbose displays the message, but only if
+                -Verbose is enabled. It can be enabled with the -Verbose
+                parameter, or by setting the $VerbosePreference variable to
+                'Continue' or another mode that displays the result.
+
+                By default, verbose messaging is NOT shown.
+            #>
+            Write-Verbose "____"
         }
 
-        It 'Provides the -Confirm common parameter' {
+        $String = "When you're falling in a forest, and there's nobody around, do you ever really crash or even make a sound?"
 
-        }
+        <#
+            By redirecting the verbose stream, we can test the result.
+            Verbose is on stream 4, and debug is on stream 5.
+        #>
+        Test-Verbose 4>&1 | Should -BeNullOrEmpty
 
-        It 'Provides the -Debug common parameter' {
+        # Verbose messages should appear with the -Verbose parameter.
+        Test-Verbose -Verbose 4>&1 | Should -Be $String
+    }
 
-        }
+    It 'provides the -WarningAction common parameter' {
 
-        It 'Provides the -WarningAction common parameter' {
+    }
 
-        }
+    It 'provides the -WarningVariable common parameter' {
 
-        It 'Provides the -WarningVariable common parameter' {
+    }
 
-        }
+    It 'provides the -ErrorAction common parameter' {
 
-        It 'Provides the -ErrorAction common parameter' {
+    }
 
-        }
+    It 'provides the -ErrorVariable common parameter' {
 
-        It 'Provides the -ErrorVariable common parameter' {
+    }
 
-        }
+    It 'provides the -InformationAction common parameter' {
 
-        It 'Provides the -InformationAction common parameter' {
+    }
 
-        }
+    It 'provides the -InformationVariable common parameter' {
 
-        It 'Provides the -InformationVariable common parameter' {
+    }
 
-        }
+    It 'provides the -OutVariable common parameter' {
 
-        It 'Provides the -OutVariable common parameter' {
+    }
 
-        }
+    It 'provides the -OutBuffer common parameter' {
 
-        It 'Provides the -OutBuffer common parameter' {
+    }
 
-        }
+    It 'provides the -PipelineVariable common parameter' {
 
-        It 'Provides the -PipelineVariable common parameter' {
-
-        }
     }
 }
