c611e926b886c6f3bef676302e7d0890fe25315b
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-06-16 23:56:01 -0400

    :rocket: Rebuild format generator file

--- PSKoans.ezformat.ps1
+++ PSKoans.ezformat.ps1
@@ -1,38 +1,37 @@
-#Requires -Modules EZOut
-# Install-Module EZOut
-# or https://github.com/StartAutomating/EZOut
-
-$VerbosePreference = 'Continue'
-
-$ModuleName = $MyInvocation.MyCommand.Name -replace '\.ezformat\.ps1', ''
-$ModuleFolder = Get-Item -Path "$PSScriptRoot/PSKoans" | Select-Object -ExpandProperty FullName
-
-Write-Verbose "Building format file for '$ModuleName' in '$ModuleFolder'"
-
+#requires -Module EZOut
+#  Install-Module EZOut or https://github.com/StartAutomating/EZOut
+$myFile = $MyInvocation.MyCommand.ScriptBlock.File
+$myModuleName = 'PSKoans'
+$myRoot = "$PSScriptRoot"
+Push-Location $myRoot
 $formatting = @(
+    # Add your own Write-FormatView here,
+    # or put them in a Formatting or Views directory
     foreach ($potentialDirectory in 'formatting', 'views') {
-        $path = Join-Path $PSScriptRoot -ChildPath $potentialDirectory
-        if (Test-Path $path) {
-            Get-ChildItem -Path $path | ForEach-Object {
-                Write-Verbose "Processing file '$($_.FullName)'"
-                Import-FormatView -FilePath $_.FullName
-            }
-        }
+        Join-Path $myRoot -ChildPath $potentialDirectory |
+            Get-ChildItem -ea ignore |
+            Import-FormatView -FilePath { $_.Fullname }
     }
 )
 
+$destinationRoot = "$PSScriptRoot/PSKoans"
+
 if ($formatting) {
-    $formatFilePath = Join-Path $ModuleFolder -ChildPath "$ModuleName.format.ps1xml"
-    Write-Verbose "Writing format file to '$formatFilePath'"
-    $formatting | Out-FormatData -Module $ModuleName | Set-Content $formatFilePath -Encoding UTF8
+    $myFormatFile = Join-Path $destinationRoot "$myModuleName.format.ps1xml"
+    $formatting | Out-FormatData -Module $MyModuleName | Set-Content $myFormatFile -Encoding UTF8
 }
 
 $types = @(
     # Add your own Write-TypeView statements here
+    # or declare them in the 'Types' directory
+    Join-Path $myRoot Types |
+        Get-Item -ea ignore |
+        Import-TypeView
+
 )
 
 if ($types) {
-    $TypesFile = Join-Path $PSScriptRoot "$ModuleName.types.ps1xml"
-    Write-Verbose "Writing types file to '$TypesFile'"
-    $types | Out-TypeData | Set-Content $TypesFile -Encoding UTF8
+    $myTypesFile = Join-Path $destinationRoot "$myModuleName.types.ps1xml"
+    $types | Out-TypeData | Set-Content $myTypesFile -Encoding UTF8
 }
+Pop-Location
