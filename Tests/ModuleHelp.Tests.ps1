a2832b4c27fa359b8de4d7fd55196facc97a9700
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-07-10 00:53:53 -0400

    :white_check_mark: fix test to count properly

--- Tests/ModuleHelp.Tests.ps1
+++ Tests/ModuleHelp.Tests.ps1
@@ -69,7 +69,7 @@ Describe "$ModuleName Sanity Tests - Help Content" -Tags 'Module' {
 
             # This will be skipped for compiled commands ($Ast.Ast will be $null)
             It "has a help entry for all parameters of $Command" -TestCases $Ast -Skip:(-not ($Parameters -and $Ast.Ast)) {
-                $Parameters.Count | Should -Be $Ast.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
+                @($Parameters).Count | Should -Be $Ast.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
             }
 
             It "has a description for $Command parameter -<Name>" -TestCases $Parameters -Skip:(-not $Parameters) {
