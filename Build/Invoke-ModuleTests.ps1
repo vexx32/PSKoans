f0464e07e79a161928df93f95605171a4c9b6903
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2020-07-17 14:16:26 -0400

    :rocket: Manually build Pester configuration

--- Build/Invoke-ModuleTests.ps1
+++ Build/Invoke-ModuleTests.ps1
@@ -12,25 +12,19 @@ Write-Host $Lines
 
 try {
     # Try/Finally required since we will exit with exit code on failure.
-    Invoke-Pester -Configuration @{
-        Run          = [Pester.RunConfiguration]@{
-            Path = "$env:PROJECTROOT/Tests"
-            Exit = $true
-        }
-        CodeCoverage = [Pester.CodeCoverageConfiguration]@{
-            Enabled = $true
-            Path    = Get-ChildItem -Recurse -Include '*.ps1' -Path @(
-                "$env:PROJECTROOT/PSKoans/PSKoans.psm1"
-                "$env:PROJECTROOT/PSKoans/Public"
-                "$env:PROJECTROOT/PSKoans/Private"
-            ) |
-                Select-Object -ExpandProperty FullName
-        }
-        TestResult   = [Pester.TestResultConfiguration]@{
-            Enabled       = $true
-            TestSuiteName = "PSKoans-Pester"
-        }
-    }
+    $configuration = [PesterConfiguration]::Default
+    $configuration.Run.Path = "$env:PROJECTROOT/Tests"
+    $configuration.Run.Exit = $true
+    $configuration.CodeCoverage.Enabled = $true
+    $configuration.CodeCoverage.Path = Get-ChildItem -Recurse -Include '*.ps1' -Path @(
+            "$env:PROJECTROOT/PSKoans/PSKoans.psm1"
+            "$env:PROJECTROOT/PSKoans/Public"
+            "$env:PROJECTROOT/PSKoans/Private"
+        ) | Select-Object -ExpandProperty FullName 
+    $configuration.TestResult.Enabled = $true
+    $configuration.TestResult.TestSuiteName = "PSKoans-Pester
+    
+    Invoke-Pester -Configuration $configuration
 }
 finally {
     $Timestamp = Get-Date -Format "yyyyMMdd-hhmmss"
