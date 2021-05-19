99e92b00ae38792d59ecada964603e103ab1d1bb
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2021-05-19 12:04:52 -0400

    :rocket: Adjust initialize-environment.ps1 for gha

--- Build/Initialize-Environment.ps1
+++ Build/Initialize-Environment.ps1
@@ -3,12 +3,27 @@ Get-PackageProvider -Name NuGet -ForceBootstrap > $null
 Set-BuildEnvironment
 
 $ProjectRoot = Resolve-Path -Path "$PSScriptRoot/.."
-Write-Host "##vso[task.setvariable variable=ProjectRoot]$ProjectRoot"
 
 $Lines = '-' * 70
 
 Write-Host $Lines
-Write-Host "Repository Branch: $env:BUILD_SOURCEBRANCHNAME ($env:BUILD_SOURCEBRANCH)"
+
+$AzurePipelines = $env:BUILD_SOURCESDIRECTORY -and $env:BUILD_BUILDNUMBER
+$GithubActions = [bool]$env:GITHUB_WORKSPACE
+
+if ($AzurePipelines) {
+    Write-Host "Repository Branch: [$env:BUILD_SOURCEBRANCHNAME] ($env:BUILD_SOURCEBRANCH)"
+    Write-Host "##vso[task.setvariable variable=ProjectRoot]$ProjectRoot"
+}
+elseif ($GithubActions) {
+    Write-Host "Repository Branch: $env:GITHUB_REF"
+    if ($env:GITHUB_HEAD_REF -and $env:GITHUB_BASE_REF) {
+        Write-Host "Pull Request Build: [$env:GITHUB_HEAD_REF] (HEAD) -> [$env:GITHUB_BASE_REF] (BASE)"
+    }
+    
+    "PROJECTROOT=$ProjectRoot" | Add-Content -Path $env:GITHUB_ENV
+}
+
 Write-Host "Build System Details:"
 Get-Item 'Env:BH*' | Out-String | Write-Host
 Write-Host $Lines
