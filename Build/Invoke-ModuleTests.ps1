362bded1d01e5a03fd8e093ad2fec92d35a229d1
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2021-05-19 09:57:03 -0400

    :rocket: Add GHA support to Invoke-ModuleTests.ps1

--- Build/Invoke-ModuleTests.ps1
+++ Build/Invoke-ModuleTests.ps1
@@ -24,13 +24,28 @@ finally {
         Get-ChildItem -Path "$env:PROJECTROOT/PSKoans" -Directory -Recurse |
             Where-Object FullName -NotMatch '[\\/]Tests[\\/]|[\\/]PSKoans[\\/]Koans[\\/]'
     ).FullName -join ';'
-
-    # Tell Azure what the test results & code coverage file names will be
-    Write-Host "##vso[task.setvariable variable=TestResults]$TestFile"
-    Write-Host "##vso[task.setvariable variable=CodeCoverageFile]$CodeCoverageFile"
-    Write-Host "##vso[task.setvariable variable=SourceFolders]$ModuleFolders"
-
-    # Move files generated from Invoke-Pester to expected location
-    Move-Item -Path './testResults.xml' -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/$TestFile"
-    Move-Item -Path './coverage.xml' -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/$CodeCoverageFile"
+    
+    $AzurePipelines = $env:BUILD_SOURCESDIRECTORY -and $env:BUILD_BUILDNUMBER
+    $GithubActions = [bool]$env:GITHUB_WORKSPACE
+    
+    if ($AzurePipelines) {
+        # Tell Azure what the test results & code coverage file names will be
+        Write-Host "##vso[task.setvariable variable=TestResults]$TestFile"
+        Write-Host "##vso[task.setvariable variable=CodeCoverageFile]$CodeCoverageFile"
+        Write-Host "##vso[task.setvariable variable=SourceFolders]$ModuleFolders"
+        
+        # Move files generated from Invoke-Pester to expected location
+        Move-Item -Path './testResults.xml' -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/$TestFile"
+        Move-Item -Path './coverage.xml' -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY/$CodeCoverageFile"
+    }
+    elseif ($GithubActions) {
+        @(
+            "TestResults=$TestFile"
+            "CodeCoverageFile=$CodeCoverageFile"
+            "SourceFolders=$ModuleFolders"
+        ) | Add-Content -Path $env:GITHUB_ENV
+        
+        Move-Item -Path './testResults.xml' -Destination "$env:GITHUB_WORKSPACE/$TestFile"
+        Move-Item -Path './coverage.xml' -Destination "$env:GITHUB_WORKSPACE/$CodeCoverageFile"        
+    }
 }
