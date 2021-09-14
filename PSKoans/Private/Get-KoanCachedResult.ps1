008b2a67220705b129f9f8f3a47ac01631136809
Chris Dent <chris@indented.co.uk>
2021-09-14 12:56:06 +0100

    Adds draft caching mechanism

---
+++ PSKoans/Private/Get-KoanCachedResult.ps1
@@ -0,0 +1,29 @@
+function Get-KoanCachedResult {
+    <#
+    .SYNOPSIS
+        Add an entry to the koan cache.
+
+    .DESCRIPTION
+        Cache the results of running tests for this koan. This avoids the overhead of repeatedly running pester on unchanged content.
+
+    .PARAMETER Path
+        The path to the koans test file.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+
+    $cacheEntryName = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+
+    if ($Script:KoanResultCache.ContainsKey($cacheEntryName)) {
+        $currentHash = (Get-FileHash -Path $Path).Hash
+        $cacheEntry = $Script:KoanResultCache[$cacheEntryName]
+
+        if ($currentHash -eq $cacheEntry['Hash']) {
+            $cacheEntry['Result']
+        }
+    }
+}
--- Start-DebugSession.ps1
+++ Start-DebugSession.ps1
@@ -6,6 +6,10 @@ $env:PSModulePath = $(
 ) -join [System.IO.Path]::PathSeparator
 
 & $PSScriptRoot/PSKoans.ezformat.ps1
+
+$pester = Get-Module pester -ListAvailable | Where-Object Version -eq '5.0.2'
+
+Import-Module (Join-Path -Path $pester.ModuleBase -ChildPath 'pester.psd1')
 Import-Module $PSScriptRoot/PSKoans
 
-### Enter code to test below
+### Enter code to test here
