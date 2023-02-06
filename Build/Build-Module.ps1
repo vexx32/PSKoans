9b1e741115546375532e653d5df5f1404bdb23a6
Beau Witter <beau.witter@gmail.com>
2023-02-06 01:33:27 -0800

    Access BuiltModulePath from env
    GitHub env acts differently when set in the yml

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -36,11 +36,30 @@ catch {
 # Build external help files from Platyps MD files
 New-ExternalHelp -Path "$env:PROJECTROOT/docs/" -OutputPath "$env:PROJECTROOT/PSKoans/en"
 
+if($null -eq $env:BUILTMODULEPATH -and $null -ne $BuiltModulePath) {
+    Copy-Item -Path "$env:PROJECTROOT/PSKoans" -Destination $BuiltModulePath -Recurse -PassThru |
+        Where-Object { -not $_.PSIsContainer }
+} elseif ($null -eq $BuiltModulePath -and $null -ne $env:BUILTMODULEPATH) {
+    Copy-Item -Path "$env:PROJECTROOT/PSKoans" -Destination $env:BUILTMODULEPATH -Recurse -PassThru |
+        Where-Object { -not $_.PSIsContainer }
+} else {
+    Write-Error "Missing BuiltModulePath environment variable."
+}
+
+Write-Host "=============================PROJECT ROOT============================="
 Get-ChildItem -Path $env:PROJECTROOT -Recurse
+
+Write-Host "=============================Built Module Path============================="
+Write-Host "BuiltModulePath: $env:BUILTMODULEPATH"
 
 Get-ChildItem -Path $env:BUILTMODULEPATH -Recurse
 
-Write-Host "BuiltModulePath: $env:BUILTMODULEPATH"
-
-Copy-Item -Path "$env:PROJECTROOT/PSKoans" -Destination $env:BUILTMODULEPATH -Recurse -PassThru |
-    Where-Object { -not $_.PSIsContainer }
+if($null -eq $env:BUILTMODULEPATH -and $null -ne $BuiltModulePath) {
+    Copy-Item -Path "$env:PROJECTROOT/PSKoans" -Destination $BuiltModulePath -Recurse -PassThru |
+        Where-Object { -not $_.PSIsContainer }
+} elseif ($null -eq $BuiltModulePath -and $null -ne $env:BUILTMODULEPATH) {
+    Copy-Item -Path "$env:PROJECTROOT/PSKoans" -Destination $env:BUILTMODULEPATH -Recurse -PassThru |
+        Where-Object { -not $_.PSIsContainer }
+} else {
+    Write-Error "Missing BuiltModulePath environment variable."
+}
